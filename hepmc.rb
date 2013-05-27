aa56e3b1e62bf79bb0d0ead291f526db96e10d49
David Hall <dhcrawley@gmail.com>
2013-05-27 19:47:47 +0100

    New formula: hepmc

---
+++ hepmc.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Hepmc < Formula
+  homepage 'http://lcgapp.cern.ch/project/simu/HepMC/'
+  url 'http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.09.tar.gz'
+  sha1 'ecb97190abedfe774629a1cbc961910b4d83b7d6'
+
+  depends_on 'cmake' => :build
+
+  def install
+    mkdir '../build' do
+      system "cmake", buildpath, "-Dmomentum:STRING=GEV", "-Dlength:STRING=MM", *std_cmake_args
+      system "make install"
+    end
+  end
+end
