7a970223f53167062d6987245b27fddfed09a9c1
David Hall <dhcrawley@gmail.com>
2013-05-28 01:30:01 +0100

    New formula: fastjet

---
+++ fastjet.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Fastjet < Formula
+  homepage 'http://fastjet.fr/'
+  url 'http://fastjet.fr/repo/fastjet-3.0.3.tar.gz'
+  sha1 'a2ff5a5026fdc0908270a27bc344fe31498fa0ee'
+
+  depends_on 'cgal' => :optional
+  option 'with-cgal', 'Enable CGAL support (required for NlnN strategy)'
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-allcxxplugins
+    ]
+
+    args << "--with-cgal=#{Formula.factory('cgal').prefix}" if build.with? "cgal"
+
+    system "./configure", *args
+    system "make install"
+
+    prefix.install 'example'
+  end
+
+  test do
+    system "#{prefix}/example/fastjet_example < #{prefix}/example/data/single-event.dat"
+  end
+end
