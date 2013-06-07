14707027cc945bf288113be3d6f27c8bc9b2be4e
David Hall <dhcrawley@gmail.com>
2013-06-07 11:26:10 +0100

    lhapdf: remove devel release due to problems where lhapdf is a dependency of a devel package: #1

--- lhapdf.rb
+++ lhapdf.rb
@@ -5,18 +5,8 @@ class Lhapdf < Formula
   url 'http://www.hepforge.org/archive/lhapdf/lhapdf-5.8.9.tar.gz'
   sha1 '9f02c2c8042811897b0d047259b0cc75e9ec3d19'
 
-  devel do
-    url 'http://www.hepforge.org/archive/lhapdf/LHAPDF-6.0.0b1.tar.gz'
-    sha1 'd6c9348774c25e4df48672cce9823f7d4b4bb45b'
-  end
-
-  if build.devel?
-    depends_on 'yaml-cpp030'
-    depends_on 'boost'
-  else
-    option 'disable-low-memory', "Warning: LHAPDF uses a lot of memory, which OS X complains about"
-    option 'with-pdf4lhc', "Only build libraries for sets following PDF4LHC recommendations"
-  end
+  option 'disable-low-memory', "Warning: LHAPDF uses a lot of memory, which OS X complains about"
+  option 'with-pdf4lhc', "Only build libraries for sets following PDF4LHC recommendations"
 
   def install
     args = %W[
@@ -25,13 +15,9 @@ class Lhapdf < Formula
       --prefix=#{prefix}
     ]
 
-    if build.devel?
-      args << "--with-yaml-cpp=#{Formula.factory('yaml-cpp030').prefix}"
-    else
-      args << "--enable-low-memory" if build.without? "disable-low-memory"
-      args << "--enable-pdfsets=cteq,mstw,nnpdf" if build.with? "pdf4lhc"
-      ENV.fortran
-    end
+    args << "--enable-low-memory" if build.without? "disable-low-memory"
+    args << "--enable-pdfsets=cteq,mstw,nnpdf" if build.with? "pdf4lhc"
+    ENV.fortran
 
     system "./configure", *args
     system "make", "install"
@@ -41,27 +27,13 @@ class Lhapdf < Formula
     system "lhapdf-config --version"
   end
 
-  def caveats;
-    if build.devel?
-      s = <<-EOS.undent
-      PDFs are packaged as downloadable tarballs. These can be installed with, e.g.
-        wget http://www.hepforge.org/archive/lhapdf/pdfsets/6.0.0/CT10nlo.tar.gz -O- | tar xz -C #{share}/LHAPDF
+  def caveats; <<-EOS.undent
+    PDF sets are downloaded using the lhapdf-getdata script.
+    Please consult the homepage for more information.
 
-      LHAPDF also searches paths in the LHA_DATA_PATH variable for
-      PDF sets installed externally.
+    LHAPDF also searches paths in the LHAPATH variable for
+    PDF sets installed externally.
 
-      EOS
-    else
-      s = <<-EOS.undent
-      PDF sets are downloaded using the lhapdf-getdata script.
-      Please consult the homepage for more information.
-
-      LHAPDF also searches paths in the LHAPATH variable for
-      PDF sets installed externally.
-
-      EOS
-    end
-
-    return s
+    EOS
   end
 end
