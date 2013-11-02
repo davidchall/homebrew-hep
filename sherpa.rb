4dc1de6a1ec8559ece630131b4c3a2652ed72563
David Hall <dhcrawley@gmail.com>
2013-11-02 10:30:31 +0000

    sherpa: Update to 2.0.0

--- sherpa.rb
+++ sherpa.rb
@@ -2,29 +2,11 @@ require 'formula'
 
 class Sherpa < Formula
   homepage 'https://sherpa.hepforge.org/'
-  url 'http://www.hepforge.org/archive/sherpa/SHERPA-MC-1.4.3.tar.gz'
-  sha1 'ba9d3fdf67057358a2194584bd8f9f4fa2c4940e'
-
-  devel do
-    url 'http://www.hepforge.org/archive/sherpa/SHERPA-MC-2.0.beta2.tar.gz'
-    sha1 '6e0908316a00194032e19e7705c837252f7a6e83'
-    version '2.0.b2'
-  end
+  url 'http://www.hepforge.org/archive/sherpa/SHERPA-MC-2.0.0.tar.gz'
+  sha1 'b1371dcb96c7cf4cea7d41fbeb4a402e2b01d05b'
 
   def patches
-    # Conform to C++ standards (to compile with clang)
-    # OS X 10.8 is now POSIX compatible with scandir signature
-    if build.devel?
-      { :p0 => [
-        "https://sherpa.hepforge.org/trac/raw-attachment/ticket/259/clang.patch",
-        "https://sherpa.hepforge.org/trac/raw-attachment/ticket/260/dirent.patch"
-        ]}
-    else
-      { :p0 => [
-        "https://sherpa.hepforge.org/trac/raw-attachment/ticket/259/143_clang.patch",
-        "https://sherpa.hepforge.org/trac/raw-attachment/ticket/260/dirent.patch"
-        ]}
-    end
+    DATA
   end
 
   depends_on 'hepmc'   => :recommended
@@ -45,16 +27,11 @@ class Sherpa < Formula
     args << "--enable-lhapdf=#{Formula.factory('lhapdf').prefix}"   if build.with? "lhapdf"
     args << "--enable-fastjet=#{Formula.factory('fastjet').prefix}" if build.with? "fastjet"
 
-    # Enable gfortran to find C++ libraries
-    ENV.append 'LDFLAGS', "-L/usr/lib -lstdc++"
-
     system "./configure", *args
     system "make", "install"
   end
 
   test do
-    system "Sherpa", "--version"
-
     (testpath/"Run.dat").write <<-EOS.undent
       (beam){
         BEAM_1 =  2212; BEAM_ENERGY_1 = 7000;
@@ -82,3 +59,47 @@ class Sherpa < Formula
     puts "Please now delete the \"Sherpa_References.tex\" file"
   end
 end
+
+
+__END__
+diff --git a/AHADIC++/Tools/Transitions.H b/AHADIC++/Tools/Transitions.H
+index 08e761d..25cf960 100644
+--- a/AHADIC++/Tools/Transitions.H
++++ b/AHADIC++/Tools/Transitions.H
+@@ -13,7 +13,7 @@
+ namespace AHADIC {
+   class Flavour_Sorting_Mass {
+   public :
+-    bool operator() (const ATOOLS::Flavour & fl1,const ATOOLS::Flavour & fl2) {
++    bool operator() (const ATOOLS::Flavour & fl1,const ATOOLS::Flavour & fl2) const {
+       if (fl1.HadMass()>fl2.HadMass()) return true;
+       return false;
+     }
+@@ -52,7 +52,7 @@ namespace AHADIC {
+   class Flavour_Pair_Sorting_Mass {
+   public :
+     bool operator() (const Flavour_Pair & flpair1,
+-		     const Flavour_Pair & flpair2) {
++		     const Flavour_Pair & flpair2) const {
+       if (//(flpair1.first==flpair2.second.Bar() &&
+ 	  //flpair1.first==flpair2.second &&
+ 	  //flpair1.second==flpair2.first.Bar() &&
+diff --git a/MODEL/Main/SM_U1_B.C b/MODEL/Main/SM_U1_B.C
+index 35f7ea2..474c52b 100644
+--- a/MODEL/Main/SM_U1_B.C
++++ b/MODEL/Main/SM_U1_B.C
+@@ -177,10 +177,10 @@ void SM_U1_B::FixMix() {
+       for (int j=0;j<3;++j) {
+ 	Complex entry=Complex(0.,0.);
+ 	for (int k=0;k<3;++k) entry += Mix[i][k]*Mixconj[k][j];
+-	if (ATOOLS::dabs(entry.real())<1.e-12) entry.real() = 0.;
+-	if (ATOOLS::dabs(entry.imag())<1.e-12) entry.imag() = 0.;
+-	if (ATOOLS::dabs(1.-entry.real())<1.e-12) entry.real() = 1.;
+-	if (ATOOLS::dabs(1.-entry.imag())<1.e-12) entry.imag() = 1.;
++	if (ATOOLS::dabs(entry.real())<1.e-12) entry = Complex(0., entry.imag());
++	if (ATOOLS::dabs(entry.imag())<1.e-12) entry = Complex(entry.real(), 0.);
++	if (ATOOLS::dabs(1.-entry.real())<1.e-12) entry = Complex(1., entry.imag());
++	if (ATOOLS::dabs(1.-entry.imag())<1.e-12) entry = Complex(entry.real(), 1.);
+ 	msg_Out()<<std::setw(os)<<entry;
+       }
+       msg_Out()<<"\n";
