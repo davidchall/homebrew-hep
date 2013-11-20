8411c73274c7227ddcad1b41e5270ffd500718a0
David Hall <dhcrawley@gmail.com>
2013-11-20 01:34:36 +0000

    APPLgrid: update to 1.4.40, required patch

--- applgrid.rb
+++ applgrid.rb
@@ -2,13 +2,17 @@ require 'formula'
 
 class Applgrid < Formula
   homepage 'http://applgrid.hepforge.org'
-  url 'http://www.hepforge.org/archive/applgrid/applgrid-1.4.28.tgz'
-  sha1 'a793e0103e338588b790444c6fa7a399c4cf92f2'
+  url 'http://www.hepforge.org/archive/applgrid/applgrid-1.4.40.tgz'
+  sha1 'c1e806775bcba8c3a6bd0e23f59a5d9a40b42720'
 
   depends_on :fortran
   depends_on 'root'
   depends_on 'hoppet' => :recommended
   depends_on 'lhapdf' => :optional
+
+  def patches
+    DATA
+  end
 
   def install
     ENV.j1
@@ -21,3 +25,18 @@ class Applgrid < Formula
     system "applgrid-config", "--version"
   end
 end
+
+__END__
+diff --git a/src/Cache.h b/src/Cache.h
+index 9f3729c..28455d7 100644
+--- a/src/Cache.h
++++ b/src/Cache.h
+@@ -84,7 +84,7 @@ public:
+       _pdf( x, Q2, &_xf[0] ); 
+ 
+       /// add to cache if enough room
+-      if ( this->size()<_max ) insert( typename _map::value_type( t, _xf ) );
++      if ( this->size()<_max ) this->insert( typename _map::value_type( t, _xf ) );
+ 
+       /// copy to output 
+       (*(partons*)xf) = ( *((partons*)&_xf[0]) ); 
