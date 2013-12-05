db3e4237b57425d5234ae22cafdc77a2df253882
David Hall <dhcrawley@gmail.com>
2013-12-05 22:26:44 +0000

    thepeg: add HEAD, improve test

--- thepeg.rb
+++ thepeg.rb
@@ -5,6 +5,14 @@ class Thepeg < Formula
   url 'http://www.hepforge.org/archive/thepeg/ThePEG-1.9.0.tar.gz'
   sha1 '7c2ede9d039fb1e78c2d520a98a0fd213a13de81'
 
+  head do
+    url 'http://thepeg.hepforge.org/hg/ThePEG', :using => :hg
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
   depends_on 'gsl'
   depends_on 'hepmc'   => :recommended
   depends_on 'rivet'   => :recommended
@@ -24,6 +32,7 @@ class Thepeg < Formula
 
     args << "--enable-stdcxx11"
 
+    system "autoreconf", "-i" if build.head?
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? 'check'
@@ -31,7 +40,8 @@ class Thepeg < Formula
   end
 
   test do
-    system "#{bin}/runThePEG --version"
+    system "setupThePEG", "#{share}/ThePEG/MultiLEP.in"
+    system "runThePEG", "MultiLEP.run"
   end
 end
 
