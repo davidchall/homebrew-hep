7069dd24be0e2371facece14710b9e4caac0acce
David Hall <dhcrawley@gmail.com>
2013-12-05 23:28:52 +0000

    herwig++: add HEAD
    Also fix dependency issue related to #1

--- herwig++.rb
+++ herwig++.rb
@@ -5,6 +5,15 @@ class Herwigxx < Formula
   url 'http://www.hepforge.org/archive/herwig/Herwig++-2.7.0.tar.gz'
   sha1 'fff87fc7ee69fedf02d2cb28d9e30b2b5e8d6862'
 
+  head do
+    url 'http://herwig.hepforge.org/hg/herwig', :using => :hg
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+    depends_on 'gengetopt'
+  end
+
   depends_on 'thepeg'
   depends_on 'hepmc'
   depends_on 'boost'
@@ -18,10 +27,10 @@ class Herwigxx < Formula
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --with-thepeg=#{Formula.factory('thepeg').prefix}
-      --with-gsl=#{Formula.factory('gsl').prefix}
+      --with-thepeg=#{Formula.factory('thepeg').opt_prefix}
     ]
 
+    system "autoreconf", "-i" if build.head?
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? 'check'
