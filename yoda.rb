2ffda06aaacb984afd781fb4eb7c872e5327eaf3
David Hall <dhcrawley@gmail.com>
2013-12-05 14:55:47 +0000

    yoda: add HEAD
    Fixes #12

--- yoda.rb
+++ yoda.rb
@@ -5,6 +5,14 @@ class Yoda < Formula
   url 'http://www.hepforge.org/archive/yoda/YODA-1.0.4.tar.gz'
   sha1 '8077b5de16e6198826014a9e01adc7cbb30df401'
 
+  head do
+    url 'http://yoda.hepforge.org/hg/yoda', :using => :hg
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
   depends_on :python
   depends_on 'cython' => :python
   depends_on 'boost'
@@ -17,6 +25,7 @@ class Yoda < Formula
       --prefix=#{prefix}
     ]
 
+    system "autoreconf", "-i" if build.head?
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? 'check'
