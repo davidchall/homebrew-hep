573802945e21d23e8b36dc1dbd9be7324124efa1
David Hall <dhcrawley@gmail.com>
2013-12-07 14:14:39 +0000

    jetvheto: fixed test
    Needed to install a PDF set. Had to rename old-style LHAPDF pdf name to
    new-style (i.e. remove .LHgrid)

--- jetvheto.rb
+++ jetvheto.rb
@@ -16,8 +16,15 @@ class Jetvheto < Formula
   end
 
   test do
-    system "#{bin}/jetvheto", "-in", "#{share}/fixed-order/H125-LHC8-R04-xmur050-xmuf050-log.fxd",
-                              "-xQ", "0.50",
-                              "-out", "H125.res"
+    # Require a PDF set
+    curl "-O", "http://www.hepforge.org/archive/lhapdf/pdfsets/6.0/unvalidated/MSTW2008nnlo68cl.tar.gz"
+    system "tar xzf MSTW2008nnlo68cl.tar.gz"
+    ENV["LHAPDF_DATA_PATH"] = Dir.pwd
+
+    cp "#{share}/fixed-order/H125-LHC8-R04-xmur050-xmuf050-log.fxd", "input.fxd"
+    inreplace "input.fxd", "MSTW2008nnlo90cl.LHgrid", "MSTW2008nnlo68cl"
+
+    system "jetvheto -in input.fxd -out output.res -xQ 0.05"
+    ohai "Successfully resummed logs for H production at the LHC"
   end
 end
