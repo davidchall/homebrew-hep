82d1918b0ecd73e93fe396f511d562a5078d7a40
David Hall <dhcrawley@gmail.com>
2013-12-07 00:16:21 +0000

    pythia8: added vincia option
    Build VINCIA as an optional resource to Pythia 8.

--- pythia8.rb
+++ pythia8.rb
@@ -8,20 +8,46 @@ class Pythia8 < Formula
 
   depends_on 'hepmc'
 
+  option 'with-vincia', 'Enable VINCIA plugin (http://vincia.hepforge.org/)'
+  if build.with? 'vincia'
+
+    resource 'vincia' do
+      url 'http://www.hepforge.org/archive/vincia/vincia-1.1.01.tgz'
+      sha1 '5ec6bff1c2e9becb391d28705db644c8262074c6'
+    end
+
+    depends_on :fortran
+    depends_on 'wget' => :build
+  end
+
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
       --enable-shared
+      --with-hepmc=#{Formula.factory('hepmc').opt_prefix}
     ]
-
-    args << "--with-hepmc=#{Formula.factory('hepmc').opt_prefix}" if build.with? "hepmc"
 
     system "./configure", *args
     system "make", "install"
 
     prefix.install 'examples'
+
+    if build.with? 'vincia'
+      resource('vincia').stage do
+        # Must tell VINCIA where to find Pythia 8 and libgfortran
+        ENV['PYTHIA8'] = buildpath
+        libgfortran = `$FC --print-file-name libgfortran.dylib`.chomp
+        system "make", "FLIBS=-L#{File.dirname libgfortran} -lgfortran"
+
+        (lib/'archive').install Dir['lib/archive/*']
+        include.install Dir['include/*']
+        (prefix/'xmldoc').install Dir['xmldoc/*']
+        prefix.install 'README.TXT' => 'README.vincia'
+        (share/'vincia').install 'antennae', 'tunes'
+      end
+    end
   end
 
   test do
