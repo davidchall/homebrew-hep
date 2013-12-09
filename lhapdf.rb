4064dd9013b5e859007d964d1104fc7ccad2d160
David Hall <dhcrawley@gmail.com>
2013-12-09 23:06:24 +0000

    lhapdf: add HEAD and fix caveats

--- lhapdf.rb
+++ lhapdf.rb
@@ -5,6 +5,15 @@ class Lhapdf < Formula
   url 'http://www.hepforge.org/archive/lhapdf/LHAPDF-6.0.4.tar.gz'
   sha1 'fba70c86d41be0be78703711ae74d22c9e4c098f'
 
+  head do
+    url 'http://lhapdf.hepforge.org/hg/lhapdf', :using => :hg
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+    depends_on 'cython' => :python
+  end
+
   depends_on 'boost'
   depends_on 'yaml-cpp'
   depends_on :python
@@ -16,6 +25,7 @@ class Lhapdf < Formula
       --prefix=#{prefix}
     ]
 
+    system "autoreconf", "-i" if build.head?
     system "./configure", *args
     system "make", "install"
   end
@@ -28,7 +38,7 @@ class Lhapdf < Formula
     PDFs are packaged as downloadable tarballs. These can be installed with, e.g.
       wget http://www.hepforge.org/archive/lhapdf/pdfsets/6.0.0/CT10nlo.tar.gz -O- | tar xz -C #{share}/LHAPDF
 
-    LHAPDF also searches paths in the LHA_DATA_PATH variable for
+    LHAPDF also searches paths in the LHAPDF_DATA_PATH variable for
     PDF sets installed externally.
 
     EOS
