423c94ef3e4b4ec2d8c32254cc97ac220ad9be5e
Enrico Bothmann <enoix@web.de>
2014-01-23 11:49:20 +0100

    Made brew audit happy and added TODO lines

--- mcgrid.rb
+++ mcgrid.rb
@@ -8,28 +8,20 @@ class Mcgrid < Formula
   depends_on "rivet"
   depends_on "applgrid"
   depends_on "boost"
-  depends_on "pkg-config" => :optional
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
 
-    # Remove unrecognized options if warned by configure
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
+
+    # TODO: Consider to copy the mcgrid example directory into share
+
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test mcgrid`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # TODO: Write test using the examples directory
+    system "true"
   end
 end
