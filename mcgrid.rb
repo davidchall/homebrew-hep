a25305484c407bcf7a49bcedaa61045bf9ca324a
Enrico Bothmann <enoix@web.de>
2014-04-01 13:16:02 +0200

    Removed now obsolete mcgrid patch

--- mcgrid.rb
+++ mcgrid.rb
@@ -9,10 +9,6 @@ class Mcgrid < Formula
   depends_on "applgrid"
   depends_on "boost"
 
-  def patches
-    DATA
-  end
-
   def install
 
     system "./configure", "--disable-dependency-tracking",
@@ -61,17 +57,3 @@ class Mcgrid < Formula
     EOS
   end
 end
-
-__END__
-diff --git a/examples/testcode/applgrid-test.cpp b/examples/testcode/applgrid-test.cpp
-index d6aa716..52df9a3 100644
---- a/examples/testcode/applgrid-test.cpp
-+++ b/examples/testcode/applgrid-test.cpp
-@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) {
-    cout <<"Applgrid information:"<<endl;
-   cout <<"Transform: "<<g.getTransform()<<endl;
-   cout <<"GenPDF: "<<g.getGenpdf()<<endl;
--  cout <<"SubProc: "<<g.subProcesses()<<endl;
-+  cout <<"SubProc: "<<g.subProcesses(nloops)<<endl;
-   cout <<"Nbins: "<<g.Nobs()<<endl;
-   cout <<endl<<"Convolution:"<<endl;
