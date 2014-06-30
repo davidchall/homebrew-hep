f73736ebf7426d54cd94f7ca4f68f9fec3793562
David Hall <dhcrawley@gmail.com>
2014-06-30 21:57:38 +0100

    thepeg: 1.9.1

--- thepeg.rb
+++ thepeg.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Thepeg < Formula
   homepage 'http://herwig.hepforge.org/'
-  url 'http://www.hepforge.org/archive/thepeg/ThePEG-1.9.0.tar.gz'
-  sha1 '7c2ede9d039fb1e78c2d520a98a0fd213a13de81'
+  url 'http://www.hepforge.org/archive/thepeg/ThePEG-1.9.1.tar.gz'
+  sha1 'bd7adfba5c5fa6f6b55bcc58958ba4dabd228a7f'
 
   head do
     url 'http://thepeg.hepforge.org/hg/ThePEG', :using => :hg
@@ -19,10 +19,6 @@ class Thepeg < Formula
   depends_on 'lhapdf'  => :recommended
   depends_on 'fastjet' => :recommended
   option 'with-check', 'Test during installation'
-
-  def patches
-    DATA
-  end
 
   def install
     args = %W[
@@ -44,18 +40,3 @@ class Thepeg < Formula
     system "runThePEG", "MultiLEP.run"
   end
 end
-
-__END__
-diff --git a/Repository/Repository.cc b/Repository/Repository.cc
-index 3e89b96..d6cc3d7 100644
---- a/Repository/Repository.cc
-+++ b/Repository/Repository.cc
-@@ -452,7 +452,7 @@ void Repository::execAndCheckReply(string line, ostream & os) {
- 
- void Repository::read(istream & is, ostream & os, string prompt) {
- #ifdef HAVE_LIBREADLINE
--  if ( is == std::cin ) {
-+  if ( &is == &std::cin ) {
-     char * line_read = 0;
-     do {
-       if ( line_read ) {
