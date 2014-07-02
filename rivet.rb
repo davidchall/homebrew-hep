38ee3329f1fac918a31704d77563b7aff498562e
David Hall <dhcrawley@gmail.com>
2014-07-02 12:15:38 +0100

    rivet: add patch
    needed for mcgrid package

--- rivet.rb
+++ rivet.rb
@@ -24,6 +24,8 @@ class Rivet < Formula
   option 'with-check', 'Test during installation'
   option 'without-analyses', 'Do not build Rivet analyses'
 
+  patch :DATA unless build.head?
+
   def install
     args = %W[
       --disable-debug
@@ -48,3 +50,23 @@ class Rivet < Formula
     ohai "Successfully ran dummy HepMC file through Drell-Yan analysis"
   end
 end
+
+__END__
+diff --git a/include/Rivet/Rivet.hh b/include/Rivet/Rivet.hh
+index d637e41..802dbc7 100644
+--- a/include/Rivet/Rivet.hh
++++ b/include/Rivet/Rivet.hh
+@@ -1,10 +1,12 @@
+ #ifndef RIVET_Rivet_HH
+ #define RIVET_Rivet_HH
+ 
++#include <string>
++
+ namespace Rivet {
+ 
+   /// A function to get the Rivet version string
+-  string version();
++  std::string version();
+ 
+ }
+ 
