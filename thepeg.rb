1f1b245c7f6ff1048213cc91069ebc4b63669c40
David Hall <dhcrawley@gmail.com>
2014-07-29 21:35:29 +0100

    thepeg: 1.9.2

--- thepeg.rb
+++ thepeg.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Thepeg < Formula
   homepage 'http://herwig.hepforge.org/'
-  url 'http://www.hepforge.org/archive/thepeg/ThePEG-1.9.1.tar.gz'
-  sha1 'bd7adfba5c5fa6f6b55bcc58958ba4dabd228a7f'
+  url 'http://www.hepforge.org/archive/thepeg/ThePEG-1.9.2.tar.gz'
+  sha1 'a5a09b90fb45e43c1e84ac55e4ee26b9bf4d55c5'
 
   head do
     url 'http://thepeg.hepforge.org/hg/ThePEG', :using => :hg
@@ -24,9 +24,8 @@ class Thepeg < Formula
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
+      --enable-stdcxx11
     ]
-
-    args << "--enable-stdcxx11"
 
     system "autoreconf", "-i" if build.head?
     system "./configure", *args
