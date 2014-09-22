6260c3f1b9cbc7521e83042a532db238651289e6
David Hall <dhcrawley@gmail.com>
2014-09-22 18:35:04 +0100

    mcgrid: 1.1
    closes #32

--- mcgrid.rb
+++ mcgrid.rb
@@ -2,15 +2,21 @@ require 'formula'
 
 class Mcgrid < Formula
   homepage 'http://mcgrid.hepforge.org'
-  url 'http://www.hepforge.org/archive/mcgrid/mcgrid-1.0.1.tar.gz'
-  sha1 'acf81099444b2ec3c632de343a22e41ce9373ea6'
+  url 'http://www.hepforge.org/archive/mcgrid/mcgrid-1.1.tar.gz'
+  sha1 '76fa7486e390d127aac3070b2f83fe301b6cce45'
 
   depends_on 'rivet'
   depends_on 'applgrid'
-  depends_on 'boost'
   depends_on 'pkg-config' => :build
 
-  patch :DATA
+  resource 'examples-rivet200' do
+    url 'http://www.hepforge.org/archive/mcgrid/MCgridExamples-2.0.0.tgz'
+    sha1 '53ecef3a3698e3c1056de64488356bb40418b362'
+  end
+  resource 'examples-rivet212' do
+    url 'http://www.hepforge.org/archive/mcgrid/MCgridExamples-2.1.2.tgz'
+    sha1 '3a68ff8d863d596c819f00c5a1053349ba565089'
+  end
 
   def install
     args = %W[
@@ -22,14 +28,27 @@ class Mcgrid < Formula
     system "./configure", *args
     system "make", "install"
 
-    prefix.install("examples")
     prefix.install("manual")
-    # Scripts for subprocess extraction are currently missing in the downloadable tarball
-    # bin.install Dir['scripts/*']
+    bin.install('scripts/identifySubprocs.py')
+
+    resource("examples-rivet200").stage {
+      (prefix/"examples-rivet-2.0.0").install Dir['*']
+    }
+    resource("examples-rivet212").stage {
+      (prefix/"examples-rivet-2.1.2").install Dir['*']
+    }
   end
 
   test do
-    examples = prefix+'examples'
+    rivet_version = `rivet-config --version`
+    if (rivet_version <=> '2.1.2') == -1 then
+      examples_suffix = '2.0.0'
+    else
+      examples_suffix = '2.1.2'
+    end
+    examples_dir = 'examples-rivet-' + examples_suffix
+
+    examples = prefix + examples_dir
     system "make -C #{examples}"
 
     ENV['RIVET_ANALYSIS_PATH'] = examples
@@ -48,55 +67,10 @@ class Mcgrid < Formula
       $(brew --prefix mcgrid)/manual
 
     Examples are installed in:
-      $(brew --prefix mcgrid)/examples
-
-    Scripts for subprocess extraction are currently missing
-    from the downloadable tarball.
+      $(brew --prefix mcgrid)/examples-rivet-2.0.0
+      $(brew --prefix mcgrid)/examples-rivet-2.1.2
+    The first set of examples is for use with rivet v2.0.0 up to (but not including) v2.1.2.
+    The second set of examples is for use with rivet v2.1.2 and newer.
     EOS
-    # Scripts for subprocess extraction are installed in:
-    #   $(brew --prefix mcgrid)/bin
-    # They are called
-    #   identify[Generator]Subprocs.py,
-    # where `Generator` is "Amegic" or "Comix".
   end
 end
-
-__END__
-diff --git a/examples/analyses/MCgrid_CDF_2009_S8383952.cc b/examples/analyses/MCgrid_CDF_2009_S8383952.cc
-index 3f58287..0289c6a 100644
---- a/examples/analyses/MCgrid_CDF_2009_S8383952.cc
-+++ b/examples/analyses/MCgrid_CDF_2009_S8383952.cc
-@@ -34,9 +34,10 @@ namespace Rivet {
-       /// Initialise and register projections here
-       // this seems to have been corrected completely for all selection cuts,
-       // i.e. eta cuts and pT cuts on leptons.
--      FinalState fs;
--      ZFinder zfinder(fs, -MAXRAPIDITY, MAXRAPIDITY, 0.0*GeV, PID::ELECTRON,
--                      66.0*GeV, 116.0*GeV, 0.2, true, true);
-+      // Patch: MAXRAPIDITY was not definied anymore in Rivet 2.1.2, so we
-+      // copied the line from the analysis in the 2.1.2 tarball.
-+      ZFinder zfinder(FinalState(), -MAXDOUBLE, MAXDOUBLE, 0*GeV, PID::ELECTRON,
-+                      66*GeV, 116*GeV, 0.2, ZFinder::CLUSTERNODECAY, ZFinder::TRACK);
-       addProjection(zfinder, "ZFinder");
-       
-       /// Book histograms here
-diff --git a/examples/testcode/applgrid-test.cpp b/examples/testcode/applgrid-test.cpp
-index 3d07d98..fce1d9f 100644
---- a/examples/testcode/applgrid-test.cpp
-+++ b/examples/testcode/applgrid-test.cpp
-@@ -99,10 +99,14 @@ int main(int argc, char* argv[]) {
-  exit(0);
- }
- 
-+// Patch: There are no FORTRAN wrappers as of LHAPDF v.6
-+#ifndef LHAPDF_MAJOR_VERSION
-+// We should be on an old LHAPDF version (below 6),
-+// which still relies on FORTRAN
- 
- #include "LHAPDF/FortranWrappers.h"
- #ifdef FC_DUMMY_MAIN
- int FC_DUMMY_MAIN() { return 1; }
- #endif
- 
--
-+#endif
