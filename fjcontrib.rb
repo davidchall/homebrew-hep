e7791f987bb356abe3c92d752d7fb4a8edd9e143
Christian Johnson <cjohnson-phys@users.noreply.github.com>
2016-02-03 20:54:33 -0500

    Update fjcontrib to v1.021

--- fjcontrib.rb
+++ fjcontrib.rb
@@ -1,7 +1,7 @@
 class Fjcontrib < Formula
   homepage 'http://fastjet.hepforge.org/contrib/'
-  url 'http://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.018.tar.gz'
-  sha256 'fe9d9661cefba048b19002abec7e75385ed73a8a3f45a29718e110beab142c2c'
+  url 'http://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.021.tar.gz'
+  sha256 'f4bcd9ca39399a3b7719358e757462ecc7937f73a6b625cbb164173a26847d33'
 
   depends_on 'fastjet'
   option 'with-check', 'Test during installation'
