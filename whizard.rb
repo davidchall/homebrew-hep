d89bb92edd3bbb98b3ad5ab0cb701e33e20afa05
David Hall <david.hall.physics@gmail.com>
2016-07-10 21:51:55 -0400

    whizard: 2.2.8

--- whizard.rb
+++ whizard.rb
@@ -1,11 +1,11 @@
 class Whizard < Formula
   desc "Monte Carlo event generator"
   homepage "http://whizard.hepforge.org"
-  url "http://www.hepforge.org/archive/whizard/whizard-2.2.6.tar.gz"
-  sha256 "5f9bcedcdcf091be8c65bb2a0d6fc47bb8e32d97b8a23aed2d33c0fd1015d275"
+  url "http://www.hepforge.org/archive/whizard/whizard-2.2.8.tar.gz"
+  sha256 "d6b5e84d5bc5bad387f842f7cb69e55a62a0a2a9d42f10fdcf76161459f03902"
 
   depends_on :fortran
-  depends_on "ocaml" => :recommended
+  depends_on "ocaml"
   depends_on "fastjet" => :optional
   depends_on "hoppet" => :optional
   depends_on "hepmc" => :optional
@@ -19,7 +19,9 @@ class Whizard < Formula
       --prefix=#{prefix}
     ]
 
-    args << "--disable-ocaml" if build.without? "ocaml"
+    # configure script finds some weird libraries that don't exist
+    ENV["CXXLIBS"] = " "
+
     args << "--enable-hoppet" if build.with? "hoppet"
     args << "--enable-fastjet" if build.with? "fastjet"
 
