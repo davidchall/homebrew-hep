e7666857056662e1f6847754bdd39fedda7c1a6b
Christian Johnson <cjohnson-phys@users.noreply.github.com>
2016-10-11 19:51:53 +0200

    Update fastjet.rb

--- fastjet.rb
+++ fastjet.rb
@@ -1,8 +1,8 @@
 class Fastjet < Formula
   desc "Package for jet finding in pp and ee collisions"
   homepage "http://fastjet.fr"
-  url "http://fastjet.fr/repo/fastjet-3.2.0.tar.gz"
-  sha256 "96a927f1a336ad93cff30f07e2dc137a4de8ff7d74d5cd43eb455f42cf5275e3"
+  url "http://fastjet.fr/repo/fastjet-3.2.1.tar.gz"
+  sha256 "c858b6c4f348c3676afa173251bb16d987674e64679a84306510e3963f858d5b"
 
   option "with-cgal", "Enable CGAL support (required for NlnN strategy)"
   option "with-test", "Test during installation"
