ca0866bb873ec9fdaa08f2330e2f013bcf918592
David C Hall <davidchall@users.noreply.github.com>
2016-10-13 15:28:50 -0400

    Update fjcontrib.rb

--- fjcontrib.rb
+++ fjcontrib.rb
@@ -2,7 +2,7 @@ class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "http://fastjet.hepforge.org/contrib/"
   url "http://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.025.tar.gz"
-  sha256 "504ff45770e8160f1d3b842ea5962c84faeb9acd2821195b08c7e21a84402dcc
+  sha256 "504ff45770e8160f1d3b842ea5962c84faeb9acd2821195b08c7e21a84402dcc"
 
   option "with-test", "Test during installation"
 
