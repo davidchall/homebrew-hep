186bf95823cfdbc4014b78a8328d38ab47f491cd
Christian Johnson <cjohnson-phys@users.noreply.github.com>
2017-02-02 13:42:40 +0100

    Update vbfnlo.rb

--- vbfnlo.rb
+++ vbfnlo.rb
@@ -1,9 +1,9 @@
 class Vbfnlo < Formula
   desc "Parton-level Monte Carlo for processes with electroweak bosons"
   homepage "https://www.itp.kit.edu/vbfnlo"
-  url "https://www.itp.kit.edu/vbfnlo/wiki/lib/exe/fetch.php?media=download:vbfnlo-3.0.0beta2.tgz"
-  version "3.0.0beta2"
-  sha256 "c4b998f7541f0b192da69ee47597f7fcdee7e537326a2542e8122cf4eed4749c"
+  url "https://www.itp.kit.edu/vbfnlo/wiki/lib/exe/fetch.php?media=download:vbfnlo-3.0.0beta3.tgz"
+  version "3.0.0beta3"
+  sha256 "bd1fb2e59fc0095bd339fc7a9d2070a078c6ee16f7b74ae5035763eea153cb58"
 
   option "with-kk", "Enable Kaluza-Klein resonances"
   option "with-spin2", "Enable spin-2 resonances"
@@ -11,7 +11,7 @@ class Vbfnlo < Formula
   depends_on :fortran
   depends_on "hepmc" => :recommended
   depends_on "lhapdf" => :recommended
-  depends_on "homebrew/science/root" => :optional
+  depends_on "homebrew/science/root6" => :optional
 
   if build.with? "kk"
     depends_on "gsl"
@@ -30,7 +30,7 @@ class Vbfnlo < Formula
     args << "--enable-spin2"                                if build.with? "spin2"
     args << "--enable-kk"                                   if build.with? "kk"
     args << "--with-LHAPDF=#{Formula["lhapdf"].opt_prefix}" if build.with? "lhapdf"
-    args << "--with-root=#{Formula["root"].opt_prefix}"     if build.with? "root"
+    args << "--with-root=#{Formula["root6"].opt_prefix}"    if build.with? "root6"
     args << "--with-hepmc=#{Formula["hepmc"].opt_prefix}"   if build.with? "hepmc"
 
     system "./configure", *args
