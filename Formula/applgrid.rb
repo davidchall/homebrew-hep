28d4cb116ca8a201134e5d3f01e988476f99496f
David Hall <dchall@mgh.harvard.edu>
2017-10-13 21:35:53 -0400

    applgrid: fix audit errors

--- Formula/applgrid.rb
+++ Formula/applgrid.rb
@@ -1,14 +1,14 @@
 class Applgrid < Formula
   desc "Quickly reproduce NLO calculations with any input PDFs"
-  homepage "http://applgrid.hepforge.org"
-  url "http://www.hepforge.org/archive/applgrid/applgrid-1.4.70.tgz"
+  homepage "https://applgrid.hepforge.org"
+  url "https://www.hepforge.org/archive/applgrid/applgrid-1.4.70.tgz"
   sha256 "37e191e0e8598b7ee486007733b99d39da081dd3411339da2468cb3d66e689fb"
   patch :DATA
 
   depends_on :fortran
   cxxstdlib_check :skip
 
-  depends_on "homebrew/science/root"
+  depends_on "root"
   depends_on "hoppet" => :recommended
   depends_on "lhapdf" => :optional
 
