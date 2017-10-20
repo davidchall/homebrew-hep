575c3474015c49e75d38e36a135fbbe9ca83b5b0
David C Hall <davidchall@users.noreply.github.com>
2017-10-20 15:26:53 -0400

    Build bottles on Travis-CI and deploy to Bintray (#114)
    * Build and upload bottles from master branch builds
    * Fix brew-audit for yoda formula

--- Formula/yoda.rb
+++ Formula/yoda.rb
@@ -1,7 +1,7 @@
 class Yoda < Formula
   desc "Yet more Objects for Data Analysis"
-  homepage "http://yoda.hepforge.org"
-  url "http://www.hepforge.org/archive/yoda/YODA-1.6.7.tar.gz"
+  homepage "https://yoda.hepforge.org"
+  url "https://www.hepforge.org/archive/yoda/YODA-1.6.7.tar.gz"
   sha256 "9fbdb8e9b2951ee6b984b2d5350b0a5a9c93c6f4a70c51dd77daa87336753cf6"
 
   head do
@@ -10,12 +10,11 @@ class Yoda < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "cython" => :python
+    depends_on "cython" => :build
   end
 
   option "with-test", "Test during installation"
 
-  depends_on :python
   depends_on "root" => :optional
   depends_on "numpy" => :optional
   depends_on "homebrew/science/matplotlib" => :optional
@@ -42,6 +41,6 @@ class Yoda < Formula
   end
 
   test do
-    system "yoda-config", "--version"
+    system bin/"yoda-config", "--version"
   end
 end
