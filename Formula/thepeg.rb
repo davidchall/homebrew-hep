f5e2b5ca4aa43bcafecae464a90d19f9a837e5a2
David Hall <dchall@mgh.harvard.edu>
2017-10-22 11:08:08 -0400

    thepeg: add bottles [skip ci]

--- Formula/thepeg.rb
+++ Formula/thepeg.rb
@@ -4,6 +4,13 @@ class Thepeg < Formula
   url "https://www.hepforge.org/archive/thepeg/ThePEG-2.1.1.tar.bz2"
   sha256 "e1b0bdc116fbc9a6e598b601f2aa670530cf2e1cd46b4572814a9b0130b10281"
 
+  bottle do
+    root_url "https://dl.bintray.com/davidchall/bottles-hep"
+    sha256 "a8382b62cc68ff5a5e4a6fe683a95c3060db2fd4a15d2513eb7bc08aded54434" => :high_sierra
+    sha256 "9313f687c0d99b20bafde60cf4c76bd788b60fc133ecbf1dfa2c372a307653db" => :sierra
+    sha256 "1b5ef62898959d5d6178b92a31802fb8dd783c699321e63bbb161792bc8e3fcc" => :el_capitan
+  end
+
   head do
     url "http://thepeg.hepforge.org/hg/ThePEG", :using => :hg
 
