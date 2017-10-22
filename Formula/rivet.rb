73569c93c5f7d78b67b54cc930891df62caee30c
David Hall <dchall@mgh.harvard.edu>
2017-10-21 23:32:55 -0400

    rivet: add bottles [skip ci]

--- Formula/rivet.rb
+++ Formula/rivet.rb
@@ -4,6 +4,14 @@ class Rivet < Formula
   url "https://www.hepforge.org/archive/rivet/Rivet-2.5.4.tar.gz"
   sha256 "2676937cecfda295c1e8597a10f0c2122b8fbb9a1473ef2906cb19a3ddefd8a1"
 
+  bottle do
+    root_url "https://dl.bintray.com/davidchall/bottles-hep"
+    sha256 "16defdcd40483c3b1d67845c25d5367c63bf31174e42670f9ac94d386d2491d7" => :high_sierra
+    sha256 "0ba3763a5036247b0814f4c8214a05f3513cac2984f9237d78f7b1a534850fe2" => :sierra
+    sha256 "d677ba0842bee2bf3268602190dea3359988a95e7255a4b2b8ad11afb9a15e8f" => :el_capitan
+    sha256 "61eeec9c28bf0c1430a1e5bbabf511c657b97d94df9c44e12064a0ad34336fa5" => :yosemite
+  end
+
   head do
     url "http://rivet.hepforge.org/hg/rivet", :using => :hg, :branch => "tip"
 
