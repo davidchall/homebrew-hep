ab4cbea384beedce76019ada30cd09dc210858ca
Christian Johnson <cjohnson-phys@users.noreply.github.com>
2018-01-28 14:46:55 +0100

    Update fjcontrib.rb

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -1,12 +1,12 @@
 class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "https://fastjet.hepforge.org/contrib/"
-  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.030.tar.gz"
-  sha256 "e4c47bf808f2c078d6fe332ccff00716fdec618e10b7adbf9344e44c8364dba9"
+  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.032.tar.gz"
+  sha256 "125bae5ab04384596c4bcde1c84a3189034f1e94fd99b6c1025cfac1c1795831"
 
   option "with-test", "Test during installation"
 
-  depends_on "fastjet"
+  depends_on "fastjet" 
 
   def install
     system "./configure", "--prefix=#{prefix}"
