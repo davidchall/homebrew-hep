45f61c94a5f0cb3382399975fa5222c03ead1b91
Enrico Bothmann <enrico.bothmann@ed.ac.uk>
2019-02-04 18:56:25 +0100

    fjcontrib: 1.041

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -1,8 +1,8 @@
 class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "https://fastjet.hepforge.org/contrib/"
-  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.030.tar.gz"
-  sha256 "e4c47bf808f2c078d6fe332ccff00716fdec618e10b7adbf9344e44c8364dba9"
+  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.041.tar.gz"
+  sha256 "31e244728de7787a582479cbed606a1c2fe5fcf3ad3ba1b0134742de5107510c"
 
   option "with-test", "Test during installation"
 
