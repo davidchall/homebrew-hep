33366889e0fcd210dea5440a99ec149995235d11
andriish <andriish@users.noreply.github.com>
2019-11-27 21:03:28 +0100

    Update hepmc3.rb
    Update

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -1,14 +1,16 @@
 class Hepmc3 < Formula
   desc "Library is to handle  event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/hepmc/"
-  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.1.2.tar.gz"
-  sha256 "4133074b3928252877982f3d4b4c6c750bb7a324eb6c7bb2afc6fa256da3ecc7"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.0.tar.gz"
+  sha256 "f132387763d170f25a7cc9f0bd586b83373c09acf0c3daa5504063ba460f89fc"
 
   option "with-test", "Test during installation"
   option "with-root", "Enable root IO"
+  option "with-python", "Enable building of python bindings"
 
   depends_on "cmake" => :build
   depends_on "root" => :optional
+  depends_on "python" => :optional
 
   def install
     mkdir "../build" do
@@ -17,6 +19,7 @@ class Hepmc3 < Formula
       ]
       args<<"-DHEPMC3_ENABLE_TEST=ON" if build.with? "test"
       args<<"-DHEPMC3_ENABLE_ROOTIO=OFF" if build.without? "root"
+      args<<"-DHEPMC3_ENABLE_PYTHON=OFF" if build.with? "python"
       system "cmake", buildpath, *args
       system "make"
       system "make", "test" if build.with? "test"
