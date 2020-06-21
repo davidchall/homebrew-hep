924fb46dcf44425c19afbee393a30004bdef79ee
Andrii Verbytskyi <averbyts@cern.ch>
2020-06-21 13:09:24 +0200

    Update HepMC3 to 3.2.2

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -1,8 +1,8 @@
 class Hepmc3 < Formula
   desc "Library is to handle  event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/hepmc/"
-  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.0.tar.gz"
-  sha256 "f132387763d170f25a7cc9f0bd586b83373c09acf0c3daa5504063ba460f89fc"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.2.tar.gz"
+  sha256 "0e8cb4f78f804e38f7d29875db66f65e4c77896749d723548cc70fb7965e2d41"
 
   option "with-test", "Test during installation"
   option "with-python", "Enable building of python bindings"
