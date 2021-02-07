ea2fa622b98dc4b75260202876ac5bcb9be19a35
Simone Amoroso <amoroso@cern.ch>
2021-02-07 19:20:37 +0100

    update Hoppet to v1.2

--- Formula/hoppet.rb
+++ Formula/hoppet.rb
@@ -1,8 +1,8 @@
 class Hoppet < Formula
   desc "QCD DGLAP evolution"
   homepage "https://hoppet.hepforge.org"
-  url "https://hoppet.hepforge.org/downloads/hoppet-1.1.5.tgz"
-  sha256 "41872b2c7ea10344ebbe4260165c24236e17c3d4b14310724eb36577b9c19402"
+  url "https://hoppet.hepforge.org/downloads/hoppet-1.2.0.tgz"
+  sha256 "bffd1bbfd3cc8d1470ded5c82fe33346d44e86cf426439eb77ab7702f319e448"
 
   option "with-test", "Test during installation"
 
