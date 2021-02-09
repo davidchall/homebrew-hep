2cabe976db6c3af98fda228f87e282517d582e1e
Simone Amoroso <amoroso@cern.ch>
2021-02-09 17:44:06 +0100

    remove empty line

--- Formula/hepmc.rb
+++ Formula/hepmc.rb
@@ -4,7 +4,6 @@ class Hepmc < Formula
   url "https://gitlab.cern.ch/hepmc/HepMC/-/archive/2.06.11/HepMC-2.06.11.tar.gz"
   sha256 "ceaced62d39e4e2a1469fa2f20662d4d370279b3209930250766db02f44ae8de"
 
-
   option "with-test", "Test during installation"
 
   depends_on "cmake" => :build
