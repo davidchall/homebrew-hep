8a6ff226c7921711a69ef2b91e73d0d46edf02a0
Simone Amoroso <amoroso@cern.ch>
2021-02-09 17:29:19 +0100

    update to HepMC 2.06.11 with weights ordering bugfix

--- Formula/hepmc.rb
+++ Formula/hepmc.rb
@@ -1,17 +1,9 @@
 class Hepmc < Formula
   desc "C++ event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/"
-  url "http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.09.tar.gz"
-  sha256 "c60724ca9740230825e06c0c36fb2ffe17ff1b1465e8656268a61dffe1611a08"
+  url "https://gitlab.cern.ch/hepmc/HepMC/-/archive/2.06.11/HepMC-2.06.11.tar.gz"
+  sha256 "ceaced62d39e4e2a1469fa2f20662d4d370279b3209930250766db02f44ae8de"
 
-  bottle do
-    root_url "https://dl.bintray.com/davidchall/bottles-hep"
-    cellar :any
-    sha256 "0f7776e2d85b80f46af012ac127499b67ed4bdbe19483704a11b373f1a6f63f6" => :high_sierra
-    sha256 "0684a190b89cd278481ad71c7c32cc48f6de15de12abd0b7736d27fd79fd21e2" => :sierra
-    sha256 "95a6f176ff49cdf95ad8a5d94f2656bf7f6557ab9672b56b04159cf03a19196c" => :el_capitan
-    sha256 "602214a37352f51cd5cdc2a57bfe4f1c79baee569d711b526a4c21d8658722f3" => :yosemite
-  end
 
   option "with-test", "Test during installation"
 
