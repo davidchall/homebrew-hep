1424c80f5ac21a390861479b14d384e507e28c93
Alois Kabelschacht <kabel@Aloiss-Air.fritz.box>
2021-02-24 18:57:15 +0100

    Added f2c and fixes to topdrawer

---
+++ Formula/f2c.rb
@@ -0,0 +1,61 @@
+class F2c < Formula
+  desc "Compiler from Fortran to C"
+  homepage "http://www.netlib.org/f2c/"
+  head "https://netlib.sandia.gov/f2c/libf2c.zip", :using => :nounzip
+  sha256 "ca404070e9ce0a9aaa6a71fc7d5489d014ade952c5d6de7efb88de8e24f2e8e0"
+
+  resource "f2cexecutablesrc" do
+    url "https://netlib.sandia.gov/f2c/src.tgz", :using => :nounzip
+    sha256 "d4847456aa91c74e5e61e2097780ca6ac3b20869fae8864bfa8dcc66f6721d35"
+  end
+
+  resource "f2cman" do
+    url "https://netlib.sandia.gov/f2c/f2c.1t", :using => :nounzip
+  end
+
+
+  def install
+    system "unzip", "libf2c.zip", "-d", "libf2c"
+    # f2c header and libf2c.a
+    cd "libf2c" do
+      system "make", "-f", "makefile.u", "f2c.h"
+      include.install "f2c.h"
+
+      system "make", "-f", "makefile.u"
+      lib.install "libf2c.a"
+    end
+
+    # f2cexecutable
+    resource("f2cexecutablesrc").stage {
+    system "tar", "zxvf", "src.tgz"
+    cd "src" do
+      system "make", "-f", "makefile.u", "f2c"
+      bin.install "f2c"
+    end
+    }
+
+    # f2cman
+    resource("f2cman").stage {
+    man1.install "f2c.1t"
+    }
+
+  end
+
+  test do
+    # check if executable doesn't error out
+    system "#{bin}/f2c", "--version"
+
+    # hello world test
+    (testpath/"test.f").write <<-EOS.undent
+      C comment line
+            program hello
+            print*, 'hello world'
+            stop
+            end
+    EOS
+    system "#{bin}/f2c", "test.f"
+    assert_predicate testpath/"test.c", :exist?
+    system "cc", "-O", "-o", "test", "test.c", "-lf2c"
+    assert_equal " hello world\n", shell_output("#{testpath}/test")
+  end
+end
