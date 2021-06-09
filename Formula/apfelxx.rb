191334c19ae6456e2b0c12d22e863d0869d68d45
Valerio Bertone <valerio.bertone@cern.ch>
2021-06-09 08:59:53 +0200

    Updating APFEL++ version

--- Formula/apfelxx.rb
+++ Formula/apfelxx.rb
@@ -1,8 +1,8 @@
 class Apfelxx < Formula
   desc "Object oriented rewriting of the APFEL evolution code"
   homepage "https://github.com/vbertone/apfelxx"
-  url "https://github.com/vbertone/apfelxx/archive/4.2.0.tar.gz"
-  sha256 "5ad23e786bf185ce4be0672f7931f6a0a817309f8fdea8e8dab6139ed8dd9a9f"
+  url "https://github.com/vbertone/apfelxx/archive/4.6.0.tar.gz"
+  sha256 "37ad4121dfa5726ebe09cd2b7cf148976feb063ec6aeae7332ad9adc29c41543"
 
   depends_on "cmake" => :build
   depends_on "gcc"
