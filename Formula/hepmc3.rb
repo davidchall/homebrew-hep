979d59d6d3ea1d763b9f3a530bf262d36902ce4c
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2022-04-09 16:03:53 +0200

    New Hepmc3 3.2.5

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -1,8 +1,8 @@
 class Hepmc3 < Formula
   desc "Library is to handle  event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/hepmc/"
-  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.2.tar.gz"
-  sha256 "0e8cb4f78f804e38f7d29875db66f65e4c77896749d723548cc70fb7965e2d41"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.5.tar.gz"
+  sha256 "cd0f75c80f75549c59cc2a829ece7601c77de97cb2a5ab75790cac8e1d585032"
 
   option "with-test", "Test during installation"
   option "with-python", "Enable building of python bindings"
