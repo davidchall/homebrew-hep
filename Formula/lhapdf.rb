f354a8d81bdd0033eb54e6c1d2e8e77a72cadf75
David C Hall <davidchall@users.noreply.github.com>
2022-04-26 15:16:46 -0700

    lhapdf 6.4.0
    Example: https://github.com/Homebrew/homebrew-core/pull/3538
    
    lhapdf 6.4.0
    
    Help configure find correct python
    
    Search for python in libexec/bin
    
    Homebrew only adds python3 to regular bin directory. The python alias is installed in libexec/bin.
    
    Add audit exception

--- Formula/lhapdf.rb
+++ Formula/lhapdf.rb
@@ -1,8 +1,10 @@
 class Lhapdf < Formula
+  include Language::Python::Shebang
+
   desc "PDF interpolation and evaluation"
   homepage "https://lhapdf.hepforge.org/"
-  url "https://www.hepforge.org/archive/lhapdf/LHAPDF-6.2.1.tar.gz"
-  sha256 "6d57ced88592bfd0feca4b0b50839110780c3a1cd158091c075a155c5917202e"
+  url "https://lhapdf.hepforge.org/downloads/?f=LHAPDF-6.4.0.tar.gz"
+  sha256 "7d2f0267e2d65b0ddee048553b342d7c893a6dbabe1e326cad62de0010dd810c"
 
   head do
     url "http://lhapdf.hepforge.org/hg/lhapdf", using: :hg
@@ -13,22 +15,28 @@ class Lhapdf < Formula
     depends_on "libtool" => :build
   end
 
-  patch :DATA
+  depends_on "python@3.9"
 
   def install
-    ENV.cxx11
-    inreplace "wrappers/python/setup.py.in", "stdc++", "c++" if ENV.compiler == :clang
+    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+    ENV.prepend_create_path "PYTHONPATH", prefix/Language::Python.site_packages("python3.9")
 
     args = %W[
-      --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
 
     system "autoreconf", "-i" if build.head?
-    system "./configure", *args
+    system "./configure", "--disable-python", *args
     system "make"
     system "make", "install"
+
+    system "./configure", "--enable-python", *args
+    cd "wrappers/python" do
+      system "python3", *Language::Python.setup_install_args(prefix)
+    end
+
+    rewrite_shebang detected_python_shebang, bin/"lhapdf"
   end
 
   def caveats
@@ -44,75 +52,7 @@ class Lhapdf < Formula
   end
 
   test do
-    system "#{bin}/lhapdf", "help"
-    system "python", "-c", "import lhapdf; lhapdf.version()"
+    system bin/"lhapdf", "--help"
+    system Formula["python@3.9"].opt_bin/"python3", "-c", "import lhapdf; lhapdf.version()"
   end
 end
-__END__
-diff --git a/bin/lhapdf.in b/bin/lhapdf.in
-index 8687cff..8582e15 100644
---- a/bin/lhapdf.in
-+++ b/bin/lhapdf.in
-@@ -271,53 +271,20 @@ def download_url(source, dest_dir, dryrun=False):
-
-     else: # URL
-         url = source
--        try:
--            import urllib.request as urllib
--        except ImportError:
--            import urllib2 as urllib
--        try:
--            u = urllib.urlopen(url)
--            file_size = int(u.info().get('Content-Length')[0])
--        except urllib.URLError:
--            e = sys.exc_info()[1]
--            logging.error('Unable to download %s' % url)
--            return False
--
-+        import urllib
-         logging.debug('Downloading from %s' % url)
-         logging.debug('Downloading to %s' % dest_filepath)
-         if dryrun:
--            logging.info('%s [%s]' % (os.path.basename(url), convertBytes(file_size)))
-             return False
--
-         try:
--            dest_file = open(dest_filepath, 'wb')
--        except IOError:
--            logging.error('Could not write to %s' % dest_filepath)
-+            urllib.urlretrieve(url, dest_filepath)
-+        except urllib.URLError:
-+            e = sys.exc_info()[1]
-+            logging.error('Error during download: ', e.reason)
-+            return False
-+        except KeyboardInterrupt:
-+            logging.error('Download halted by user')
-             return False
--        try:
--            try:
--                file_size_dl = 0
--                buffer_size  = 8192
--                while True:
--                    buffer = u.read(buffer_size)
--                    if not buffer: break
--
--                    file_size_dl += len(buffer)
--                    dest_file.write(buffer)
--
--                    status  = chr(13) + '%s: ' % os.path.basename(url)
--                    status += r'%s [%3.1f%%]' % (convertBytes(file_size_dl).rjust(10), file_size_dl * 100. / file_size)
--                    sys.stdout.write(status+' ')
--            except urllib.URLError:
--                e = sys.exc_info()[1]
--                logging.error('Error during download: ', e.reason)
--                return False
--            except KeyboardInterrupt:
--                logging.error('Download halted by user')
--                return False
--        finally:
--            dest_file.close()
--            print('')
-
-     return True
-
-
