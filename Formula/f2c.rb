9b5a371504c6af66d2bd6223102e4ce7eec82883
David C Hall <davidchall@users.noreply.github.com>
2022-04-25 22:09:43 -0700

    Fix audit errors for f2c

--- Formula/f2c.rb
+++ Formula/f2c.rb
@@ -1,8 +1,11 @@
 class F2c < Formula
   desc "Compiler from Fortran to C"
   homepage "http://www.netlib.org/f2c/"
+  url "https://netlib.sandia.gov/f2c/libf2c.zip", using: :nounzip
+  # brew audit errors on head-only formula
+  # as a hack, we set version to YYYY.MM.DD
+  version "2022.04.25"
   sha256 "ca404070e9ce0a9aaa6a71fc7d5489d014ade952c5d6de7efb88de8e24f2e8e0"
-  head "https://netlib.sandia.gov/f2c/libf2c.zip", using: :nounzip
 
   resource "f2cexecutablesrc" do
     url "https://netlib.sandia.gov/f2c/src.tgz", using: :nounzip
@@ -11,6 +14,7 @@ class F2c < Formula
 
   resource "f2cman" do
     url "https://netlib.sandia.gov/f2c/f2c.1t", using: :nounzip
+    sha256 "d37a61295b245906a81d262a667f2a4491f5bd9243b07d46a6fb2e1689e8e8e5"
   end
 
   def install
