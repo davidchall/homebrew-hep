1f6c9ff2e4157f813d0c8b9a2a32de2a4e279b79
David C Hall <davidchall@users.noreply.github.com>
2022-04-29 20:18:13 -0700

    Fix test for hepmc3

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -1,6 +1,6 @@
 class Hepmc3 < Formula
-  desc "Library is to handle  event record for Monte Carlo generators"
-  homepage "https://hepmc.web.cern.ch/hepmc/"
+  desc "C++ event record for Monte Carlo generators"
+  homepage "https://hepmc.web.cern.ch/"
   url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.5.tar.gz"
   sha256 "cd0f75c80f75549c59cc2a829ece7601c77de97cb2a5ab75790cac8e1d585032"
 
@@ -18,9 +18,11 @@ class Hepmc3 < Formula
         -DCMAKE_INSTALL_PREFIX=#{prefix}
         -DHEPMC3_INSTALL_INTERFACES=ON
       ]
-      args<<"-DHEPMC3_ENABLE_TEST=ON" if build.with? "test"
-      args<<"-DHEPMC3_ENABLE_PYTHON=OFF" if build.without? "python"
-      args<<"-DHEPMC3_ENABLE_ROOTIO=OFF" if build.without? "root"
+
+      args << "-DHEPMC3_ENABLE_TEST=ON" if build.with? "test"
+      args << "-DHEPMC3_ENABLE_PYTHON=OFF" if build.without? "python"
+      args << "-DHEPMC3_ENABLE_ROOTIO=OFF" if build.without? "root"
+
       system "cmake", buildpath, *args
       system "make"
       system "make", "test" if build.with? "test"
@@ -29,6 +31,9 @@ class Hepmc3 < Formula
   end
 
   test do
-    system "make", "test"
+    cp_r share/"doc/HepMC3/examples/.", testpath
+    system "cmake", "-DUSE_INSTALLED_HEPMC3=ON", "CMakeLists.txt"
+    system "make", "basic_tree.exe"
+    system "outputs/bin/basic_tree.exe"
   end
 end
