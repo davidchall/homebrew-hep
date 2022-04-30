877511db844fd7fa118d49d338eda494b8a8fa2b
David C Hall <davidchall@users.noreply.github.com>
2022-04-29 20:53:26 -0700

    fjcontrib 1.048

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -1,8 +1,8 @@
 class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "https://fastjet.hepforge.org/contrib/"
-  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.041.tar.gz"
-  sha256 "31e244728de7787a582479cbed606a1c2fe5fcf3ad3ba1b0134742de5107510c"
+  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.048.tar.gz"
+  sha256 "f9989d3b6aeb22848bcf91095c30607f027d3ef277a4f0f704a8f0fc2e766981"
 
   option "with-test", "Test during installation"
 
@@ -13,5 +13,9 @@ class Fjcontrib < Formula
     system "make"
     system "make", "check" if build.with? "test"
     system "make", "install"
+  end
+
+  test do
+    system "true"
   end
 end
