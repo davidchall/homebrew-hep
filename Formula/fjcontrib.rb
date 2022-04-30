d35ea491dd06af9ce4ca37edf97af7c62d0836d3
David C Hall <davidchall@users.noreply.github.com>
2022-04-29 22:32:40 -0700

    Add real test

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -3,6 +3,7 @@ class Fjcontrib < Formula
   homepage "https://fastjet.hepforge.org/contrib/"
   url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.048.tar.gz"
   sha256 "f9989d3b6aeb22848bcf91095c30607f027d3ef277a4f0f704a8f0fc2e766981"
+  revision 1
 
   option "with-test", "Test during installation"
 
@@ -16,6 +17,30 @@ class Fjcontrib < Formula
   end
 
   test do
-    system "true"
+    (testpath/"test.cpp").write <<~EOS
+      #include <vector>
+      #include "fastjet/PseudoJet.hh"
+      #include "fastjet/ClusterSequence.hh"
+      #include "fastjet/contrib/SoftDrop.hh"
+      int main() {
+        std::vector<fastjet::PseudoJet> particles{{1,1,0,2}, {1,-1,0,2}};
+        fastjet::ClusterSequence cs(particles, fastjet::JetDefinition(fastjet::antikt_algorithm, 0.4));
+        fastjet::contrib::SoftDrop sd(0.5, 0.1);
+        for (const fastjet::PseudoJet& j : cs.inclusive_jets()) {
+          const fastjet::PseudoJet& jsd = sd(j);
+        }
+        return 0;
+      }
+    EOS
+
+    flags = [
+      "-I#{Formula["fastjet"].opt_include}",
+      "-I#{opt_include}",
+      "-L#{Formula["fastjet"].opt_lib}", "-lfastjet", "-lfastjettools",
+      "-L#{opt_lib}", "-lRecursiveTools"
+    ]
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system "./test"
   end
 end
