6af6b0610e4e526ae0a0a81fecd3b6ae8fd781d6
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-04-30 17:49:53 +0000

    yoda: add 1.9.4 bottle.

--- Formula/yoda.rb
+++ Formula/yoda.rb
@@ -6,6 +6,14 @@ class Yoda < Formula
   url "https://yoda.hepforge.org/downloads/?f=YODA-1.9.4.tar.gz"
   sha256 "02d89e37fbe01c5b19140e7ded9082930f76bf3d15a6ff9742fd52c8f0e738d7"
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    rebuild 1
+    sha256 cellar: :any, monterey: "7d8066c9493267eb51dd2cf9df480723ef4e210643d33c2c4eba332722d69496"
+    sha256 cellar: :any, big_sur:  "31e667ee68972ee0427ce8d2a99bdf439c48a6cbee290e68e798dc3e3d15b174"
+    sha256 cellar: :any, catalina: "a3c03d1f831a4e9cfe9519abf5b07a55029cf56de1766b453910e12fadd4e25c"
+  end
+
   head do
     url "http://yoda.hepforge.org/hg/yoda", using: :hg
 
