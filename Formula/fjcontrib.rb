faf0514e09202f2ae45919153882c161eefd41f1
David C Hall <davidchall@users.noreply.github.com>
2022-04-29 20:53:26 -0700

    fjcontrib 1.048
    * fjcontrib 1.048
    * Add real test
    
    Closes #191.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -1,8 +1,9 @@
 class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "https://fastjet.hepforge.org/contrib/"
-  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.041.tar.gz"
-  sha256 "31e244728de7787a582479cbed606a1c2fe5fcf3ad3ba1b0134742de5107510c"
+  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.048.tar.gz"
+  sha256 "f9989d3b6aeb22848bcf91095c30607f027d3ef277a4f0f704a8f0fc2e766981"
+  revision 1
 
   option "with-test", "Test during installation"
 
@@ -13,5 +14,33 @@ class Fjcontrib < Formula
     system "make"
     system "make", "check" if build.with? "test"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <vector>
+      #include "fastjet/PseudoJet.hh"
+      #include "fastjet/ClusterSequence.hh"
+      #include "fastjet/contrib/SoftDrop.hh"
+      int main() {
+        std::vector<fastjet::PseudoJet> particles{{1,1,0,2}, {1,-1,0,2}};
+        fastjet::ClusterSequence cs(particles, fastjet::JetDefinition(fastjet::antikt_algorithm, 0.4));
+        fastjet::contrib::SoftDrop sd(0.5, 0.1);
+        for (const fastjet::PseudoJet& j : cs.inclusive_jets()) {
+          const fastjet::PseudoJet& jsd = sd(j);
+        }
+        return 0;
+      }
+    EOS
+
+    flags = [
+      "-I#{Formula["fastjet"].opt_include}",
+      "-I#{opt_include}",
+      "-L#{Formula["fastjet"].opt_lib}", "-lfastjet", "-lfastjettools",
+      "-L#{opt_lib}", "-lRecursiveTools"
+    ]
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system "./test"
   end
 end
