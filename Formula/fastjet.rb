f84d9c34ffeda98fd0b645faeb61c2ffab5b6626
David C Hall <davidchall@users.noreply.github.com>
2022-04-29 17:14:17 -0700

    fastjet 3.4.0

--- Formula/fastjet.rb
+++ Formula/fastjet.rb
@@ -1,12 +1,13 @@
 class Fastjet < Formula
   desc "Package for jet finding in pp and ee collisions"
   homepage "http://fastjet.fr"
-  url "http://fastjet.fr/repo/fastjet-3.3.2.tar.gz"
-  sha256 "3f59af13bfc54182c6bb0b0a6a8541b409c6fda5d105f17e03c4cce8db9963c2"
+  url "http://fastjet.fr/repo/fastjet-3.4.0.tar.gz"
+  sha256 "ee07c8747c8ead86d88de4a9e4e8d1e9e7d7614973f5631ba8297f7a02478b91"
 
   option "without-cgal", "Disable CGAL support (required for NlnN strategy)"
   option "with-test", "Test during installation"
 
+  depends_on "python@3.9"
   depends_on "cgal" => :recommended
 
   def install
@@ -23,9 +24,6 @@ class Fastjet < Formula
     system "./configure", *args
     system "make"
     system "make", "check" if build.with? "test"
-
-    # make install fails with multiple jobs
-    ENV.deparallelize
     system "make", "install"
 
     prefix.install "example"
@@ -34,9 +32,9 @@ class Fastjet < Formula
   test do
     ln_s prefix/"example/data", testpath
     ln_s prefix/"example/python", testpath
-    system "#{prefix}/example/fastjet_example < data/single-event.dat"
+    system prefix/"example/fastjet_example < data/single-event.dat"
     cd "python" do
-      system "python", "01-basic.py"
+      system Formula["python@3.9"].opt_bin/"python3", "01-basic.py"
     end
   end
 end
