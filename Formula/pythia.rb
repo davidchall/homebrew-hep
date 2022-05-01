38a6e48a65b3b975c93fad22753b8ede298fc45e
David C Hall <davidchall@users.noreply.github.com>
2022-05-01 07:29:13 -0700

    pythia: 8.307
    Closes #195.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/pythia.rb
+++ Formula/pythia.rb
@@ -1,30 +1,28 @@
 class Pythia < Formula
   desc "Monte Carlo event generator"
-  homepage "http://home.thep.lu.se/~torbjorn/Pythia.html"
-  url "https://pythia.org/download/pythia82/pythia8243.tgz"
-  version "8.243"
-  sha256 "067219fb737bfd379ddb602b609ff5cd04f0569782f8d9d59e403264fd8a21f3"
+  homepage "https://pythia.org"
+  url "https://pythia.org/download/pythia83/pythia8307.tgz"
+  version "8.307"
+  sha256 "e5b14d44aa5943332e32dd5dda9a18fdd1a0085c7198e28d840e04167fa6013d"
 
   depends_on "boost"
-  depends_on "hepmc"
+  depends_on "hepmc3"
   depends_on "lhapdf"
 
   def install
     args = %W[
       --prefix=#{prefix}
-      --enable-shared
-      --with-hepmc2=#{Formula["hepmc"].opt_prefix}
+      --with-hepmc3=#{Formula["hepmc3"].opt_prefix}
       --with-lhapdf6=#{Formula["lhapdf"].opt_prefix}
       --with-boost=#{Formula["boost"].opt_prefix}
     ]
 
     system "./configure", *args
+    system "make"
     system "make", "install"
   end
 
   test do
-    ENV["PYTHIA8DATA"] = share/"Pythia8/xmldoc"
-
     cp_r share/"Pythia8/examples/.", testpath
     system "make", "main01"
     system "./main01"
