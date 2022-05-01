04d905348aa094c2ce8085b4644dec1f083aabe3
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-05-01 14:53:23 +0000

    pythia: add 8.307 bottle.

--- Formula/pythia.rb
+++ Formula/pythia.rb
@@ -5,6 +5,13 @@ class Pythia < Formula
   version "8.307"
   sha256 "e5b14d44aa5943332e32dd5dda9a18fdd1a0085c7198e28d840e04167fa6013d"
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 monterey: "1324fce29466c5f53bb3139598c90cd17939e5b26a499d7cdd8d9e3e9923d7ae"
+    sha256 big_sur:  "4b620e01e9684de0f59348176aa57d9389f634918c13f171c9c1f4053964986d"
+    sha256 catalina: "1e053044666bf492bb748e187ea581e7b12523424862a884d3d207e32a0337c5"
+  end
+
   depends_on "boost"
   depends_on "hepmc3"
   depends_on "lhapdf"
