53022e80c9bd8c7c165cfbc85b9ae32da738b8b0
David C Hall <davidchall@users.noreply.github.com>
2022-05-02 09:04:34 -0700

    sherpa: 2.2.11
    Remove optional dependencies to get formula working again. These can always be added again in future.

--- Formula/sherpa.rb
+++ Formula/sherpa.rb
@@ -1,70 +1,31 @@
 class Sherpa < Formula
   desc "Monte Carlo event generator"
-  homepage "https://sherpa.hepforge.org"
-  url "https://sherpa.hepforge.org/downloads/?f=SHERPA-MC-2.2.6.tar.gz"
-  sha256 "f114e68ed610ff80b772d5ee50fb1cd129d0219f38f0825cbb80ca1bfe9c4fb1"
+  homepage "https://sherpa-team.gitlab.io"
+  url "https://sherpa.hepforge.org/downloads/?f=SHERPA-MC-2.2.11.tar.gz"
+  sha256 "0eb03f87f7ff3231b294ac40b5532ae8e2ef11d6fac81ee946df14257366c22d"
 
-  # Requires changes to MCFM code, so cannot use MCFM formula
-  option "with-mcfm", "Enable use of MCFM loops"
-  depends_on "gnu-sed" => :build if build.with? "mcfm"
-
+  depends_on "autoconf" => :build
+  depends_on "fastjet"
   depends_on "gcc" # for gfortran
+  depends_on "hepmc3"
+  depends_on "lhapdf"
   depends_on "sqlite" # configure script does not work with system sqlite
-  depends_on "fastjet"   => :recommended
-  depends_on "hepmc"     => :recommended
-  depends_on "lhapdf"    => :recommended
-  depends_on "openloops" => :recommended
-  depends_on "rivet"     => :recommended
-  depends_on "open-mpi"  => :optional
-  depends_on "root"      => :optional
 
   def install
-    ENV.cxx11 if build.with?("rivet") || build.with?("lhapdf")
-
-    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
       --enable-analysis
       --enable-gzip
+      --enable-fastjet=#{Formula["fastjet"].opt_prefix}
+      --enable-hepmc3=#{Formula["hepmc3"].opt_prefix}
+      --enable-lhapdf=#{Formula["lhapdf"].opt_prefix}
+      --with-sqlite3=#{Formula["sqlite"].opt_prefix}
     ]
-    args << "--with-sqlite3=#{Formula["sqlite"].opt_prefix}"
-
-    if build.with? "open-mpi"
-      args << "--enable-mpi"
-      ENV["CC"] = ENV["MPICC"]
-      ENV["CXX"] = ENV["MPICXX"]
-      ENV["FC"] = ENV["MPIFC"]
-    end
-
-    args << "--enable-hepmc2=#{Formula["hepmc"].opt_prefix}"        if build.with? "hepmc"
-    args << "--enable-lhapdf=#{Formula["lhapdf"].opt_prefix}"       if build.with? "lhapdf"
-    args << "--enable-fastjet=#{Formula["fastjet"].opt_prefix}"     if build.with? "fastjet"
-    args << "--enable-openloops=#{Formula["openloops"].opt_prefix}" if build.with? "openloops"
-    args << "--enable-root=#{Formula["root"].opt_prefix}"           if build.with? "root"
-    args << "--enable-rivet=#{Formula["rivet"].opt_prefix}"         if build.with? "rivet"
-
-    if build.with? "mcfm"
-      mcfm_path = buildpath/"mcfm"
-      mcfm_path.mkdir
-      cd mcfm_path do
-        # MCFM build sometimes fails due to race condition
-        ENV.deparallelize
-        # install script uses wget, remove this dependency
-        inreplace "#{buildpath}/AddOns/MCFM/install_mcfm.sh", "wget", "curl -O"
-        # install script uses GNU extensions to sed
-        inreplace "#{buildpath}/AddOns/MCFM/install_mcfm.sh", "sed", "gsed"
-        system "#{buildpath}/AddOns/MCFM/install_mcfm.sh"
-        # there is no ENV.parallelize
-        ENV["MAKEFLAGS"] = "-j#{ENV.make_jobs}"
-        args << "--enable-mcfm=#{mcfm_path}"
-      end
-    end
 
     system "./configure", *args
+    system "make"
     system "make", "install"
-
-    bash_completion.install share/"SHERPA-MC/sherpa-completion"
   end
 
   test do
@@ -90,7 +51,6 @@ class Sherpa < Formula
       }(mi)
     EOS
 
-    system "#{bin}/Sherpa", "-p", testpath, "-L", testpath, "-e", "1000"
-    ohai "You just successfully generated 1000 Drell-Yan events!"
+    system bin/"Sherpa", "-p", testpath, "-L", testpath, "-e", "100"
   end
 end
