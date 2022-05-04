3ad2c3234197041d48cc08a2f93aaf060b6bad47
David C Hall <davidchall@users.noreply.github.com>
2022-05-03 21:23:04 -0700

    Rename hepmc3 as hepmc; Remove hepmc
    HepMC2 development stopped a long time ago. The main suite of homebrew-hep software has upgraded to HepMC3.

--- Formula/hepmc.rb
+++ Formula/hepmc.rb
@@ -1,16 +1,38 @@
 class Hepmc < Formula
   desc "C++ event record for Monte Carlo generators"
   homepage "https://hepmc.web.cern.ch/"
-  url "https://gitlab.cern.ch/hepmc/HepMC/-/archive/2.06.11/HepMC-2.06.11.tar.gz"
-  sha256 "ceaced62d39e4e2a1469fa2f20662d4d370279b3209930250766db02f44ae8de"
+  url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.5.tar.gz"
+  sha256 "cd0f75c80f75549c59cc2a829ece7601c77de97cb2a5ab75790cac8e1d585032"
+
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 cellar: :any, monterey: "712a1f121f0068b8e728edc70f367a8e73e2b127541000a7439f340391aa2479"
+    sha256 cellar: :any, big_sur:  "83680442908866dc286291b8158d195b8eb42b73fa7bbcf545929a4e3908be6f"
+    sha256 cellar: :any, catalina: "8859f256ea68856522ac217017de97b12528f7b6e2f756fd3ab66e7f61cc6cc7"
+  end
 
   option "with-test", "Test during installation"
+  option "with-root", "Enable root IO"
 
-  depends_on "cmake" => :build
+  depends_on "cmake" => [:build, :test]
+  depends_on "coreutils" # HepMC-config uses greadlink
+  depends_on "python@3.9"
+  depends_on "root" => :optional
 
   def install
     mkdir "../build" do
-      system "cmake", buildpath, "-Dmomentum:STRING=GEV", "-Dlength:STRING=MM", *std_cmake_args
+      args = %W[
+        -DCMAKE_INSTALL_PREFIX=#{prefix}
+        -DHEPMC3_INSTALL_INTERFACES=ON
+        -DHEPMC3_BUILD_STATIC_LIBS=OFF
+        -DHEPMC3_PYTHON_VERSIONS=3.9
+        -DHEPMC3_Python_SITEARCH39=#{prefix/Language::Python.site_packages("python3.9")}
+      ]
+
+      args << "-DHEPMC3_ENABLE_TEST=ON" if build.with? "test"
+      args << "-DHEPMC3_ENABLE_ROOTIO=OFF" if build.without? "root"
+
+      system "cmake", buildpath, *args
       system "make"
       system "make", "test" if build.with? "test"
       system "make", "install"
@@ -18,8 +40,14 @@ class Hepmc < Formula
   end
 
   test do
-    cp_r share/"HepMC/examples/.", testpath
-    system "make", "example_BuildEventFromScratch.exe"
-    system "./example_BuildEventFromScratch.exe"
+    system bin/"HepMC3-config", "--prefix"
+
+    python = Formula["python@3.9"].opt_bin/"python3"
+    system python, "-c", "import pyHepMC3"
+
+    cp_r share/"doc/HepMC3/examples/.", testpath
+    system "cmake", "-DUSE_INSTALLED_HEPMC3=ON", "CMakeLists.txt"
+    system "make", "basic_tree.exe"
+    system "outputs/bin/basic_tree.exe"
   end
 end
