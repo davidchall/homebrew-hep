876bfded76311e411bd4b60389132345bcf6376a
David C Hall <davidchall@users.noreply.github.com>
2022-05-03 21:44:33 -0700

    hepmc: add bottles

--- Formula/hepmc.rb
+++ Formula/hepmc.rb
@@ -3,6 +3,7 @@ class Hepmc < Formula
   homepage "https://hepmc.web.cern.ch/"
   url "https://hepmc.web.cern.ch/hepmc/releases/HepMC3-3.2.5.tar.gz"
   sha256 "cd0f75c80f75549c59cc2a829ece7601c77de97cb2a5ab75790cac8e1d585032"
+  license "GPL-3.0-or-later"
 
   option "with-test", "Test during installation"
   option "with-root", "Enable root IO"
