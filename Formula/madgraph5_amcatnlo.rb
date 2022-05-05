e87e60e2525d81e9b5e2fb1ad9ed02f432401ea7
David C Hall <davidchall@users.noreply.github.com>
2022-05-04 22:23:55 -0700

    madgraph5_amcatnlo 3.3.2
    * mg5_aMC: 3.3.2
    * Latest six
    
    Closes #215.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/madgraph5_amcatnlo.rb
+++ Formula/madgraph5_amcatnlo.rb
@@ -1,43 +1,48 @@
 class Madgraph5Amcatnlo < Formula
+  include Language::Python::Shebang
+
   desc "Automated LO and NLO processes matched to parton showers"
   homepage "https://launchpad.net/mg5amcnlo"
-  url "https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.7.tar.gz"
-  sha256 "d4db991a4b2016b7d84bc9e27514757f993916354c253111402c390785de4cb1"
+  url "https://launchpad.net/mg5amcnlo/3.0/3.3.x/+download/MG5_aMC_v3.3.2.tar.gz"
+  sha256 "d62f020d5b1bf72f654acc96916aba895bdc50f88ad560080f0e63894201ed99"
+  license "NCSA"
 
   depends_on "fastjet"
   depends_on "gcc" # for gfortran
+  depends_on "python@3.9"
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
 
   def install
+    resource("six").stage do
+      system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    end
+
     # fix broken dynamic links
+    gfortran_lib = Formula["gcc"].opt_lib/"gcc"/Formula["gcc"].version_suffix
     MachO::Tools.change_install_name("vendor/DiscreteSampler/check",
                                      "/opt/local/lib/libgcc/libgfortran.3.dylib",
-                                     "#{Formula["gcc"].lib}/gcc/#{Formula["gcc"].version_suffix}/libgfortran.dylib")
+                                     "#{gfortran_lib}/libgfortran.dylib")
     MachO::Tools.change_install_name("vendor/DiscreteSampler/check",
                                      "/opt/local/lib/libgcc/libquadmath.0.dylib",
-                                     "#{Formula["gcc"].lib}/gcc/#{Formula["gcc"].version_suffix}/libquadmath.0.dylib")
+                                     "#{gfortran_lib}/libquadmath.0.dylib")
 
-    cp_r ".", prefix
+    prefix.install Dir["*"]
 
     # Homebrew deletes empty directories, but aMC@NLO needs them
     Dir["**/"].reverse_each { |d| touch prefix/d/".keepthisdir" if Dir.entries(d).sort==%w[. ..] }
-  end
 
-  def caveats
-    <<~EOS
-      To shower aMC@NLO events with herwig++ or pythia8, first install
-      them via homebrew and then enter in the mg5_aMC interpreter:
-
-        set hepmc_path #{HOMEBREW_PREFIX}
-        set thepeg_path #{HOMEBREW_PREFIX}
-        set hwpp_path #{HOMEBREW_PREFIX}
-        set pythia8_path #{HOMEBREW_PREFIX}
-
-    EOS
+    rewrite_shebang detected_python_shebang, bin/"mg5_aMC"
   end
 
   test do
-    system "echo \'generate p p > t t~\' >> test.mg5"
-    system "echo \'quit\' >> test.mg5"
-    system "#{bin}/mg5_aMC", "-f", "test.mg5"
+    (testpath/"test.mg5").write <<~EOS
+      generate p p > t t~
+      quit
+    EOS
+    system bin/"mg5_aMC", "-f", "test.mg5"
   end
 end
