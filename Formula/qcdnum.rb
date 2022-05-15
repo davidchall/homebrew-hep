fd6daec9cbdf095ce9890341551ef82d456533a3
David C Hall <davidchall@users.noreply.github.com>
2022-05-15 10:12:06 -0700

    add license

--- Formula/qcdnum.rb
+++ Formula/qcdnum.rb
@@ -4,6 +4,7 @@ class Qcdnum < Formula
   url "https://www.nikhef.nl/~h24/qcdnum-files/download/qcdnum180000.tar.gz"
   version "18.0.0"
   sha256 "e108f926b7840352e4080ba71914d3403ed8118364f87710e221fdec320ee200"
+  license "GPL-3.0-or-later"
 
   depends_on "gcc" # for gfortran
 
