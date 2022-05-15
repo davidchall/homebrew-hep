3c36df084e0b60bdc6536933121d5706c83dfecd
David C Hall <davidchall@users.noreply.github.com>
2022-05-15 10:06:27 -0700

    qcdnum: 18.0.0

--- Formula/qcdnum.rb
+++ Formula/qcdnum.rb
@@ -1,8 +1,9 @@
 class Qcdnum < Formula
   desc "Fast PDF evolution"
   homepage "https://www.nikhef.nl/~h24/qcdnum"
-  url "https://www.nikhef.nl/user/h24/qcdnum-files/download/qcdnum170007.tar.gz"
-  sha256 "768a8cd4d2f140f8ee0d1ba886bc72f872b39a69b6ef16f890c1044295ce31af"
+  url "https://www.nikhef.nl/~h24/qcdnum-files/download/qcdnum180000.tar.gz"
+  version "18.0.0"
+  sha256 "e108f926b7840352e4080ba71914d3403ed8118364f87710e221fdec320ee200"
 
   depends_on "gcc" # for gfortran
 
@@ -30,17 +31,15 @@ class Qcdnum < Formula
       -Wall
       -O
       -fbounds-check
-      "#{prefix}/testjobs/example.f"
+      #{prefix}/testjobs/example.f
       -o
       example.exe
-      "#{lib}/libhqstf.a"
-      "#{lib}/libzmstf.a"
-      "#{lib}/libqcdnum.a"
-      "#{lib}/libmbutil.a"
+      #{lib}/libhqstf.a
+      #{lib}/libzmstf.a
+      #{lib}/libqcdnum.a
+      #{lib}/libmbutil.a
     ]
     system "gfortran", *args
     system "./example.exe"
-
-    ohai "Test program worked fine. Use 'brew test -v qcdnum' to watch it work"
   end
 end
