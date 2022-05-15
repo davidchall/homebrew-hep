9e11fc51a23e62350c11c8e2d731fe3ca2b87bc9
David C Hall <davidchall@users.noreply.github.com>
2022-05-15 15:02:19 -0700

    fastnlo: 2.5.0
    Closes #219.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/fastnlo.rb
+++ Formula/fastnlo.rb
@@ -1,20 +1,27 @@
 class Fastnlo < Formula
   desc "Fast pQCD calculations for PDF fits"
   homepage "https://fastnlo.hepforge.org"
-  url "https://fastnlo.hepforge.org/code/v23/fastnlo_toolkit-2.3.1pre-2212.tar.gz"
-  version "2.3.1.2212"
-  sha256 "f7d16524db1e18cd5ee5fb493f0872ae4dc4a448e758d8ccdf5c090018b7f675"
+  url "https://fastnlo.hepforge.org/code/v25/fastnlo_toolkit-2.5.0-2826.tar.gz"
+  version "2.5.0"
+  sha256 "eda20c6023a41c2ff5ec21d88847ebc6fb522714c38afad2ec1437d9c1a2132d"
+  license "GPL-3.0-only"
+
+  option "with-test", "Test during installation"
 
   depends_on "lhapdf"
-  depends_on "fastjet" => :optional
+  depends_on "fastjet" => :recommended
   depends_on "hoppet"  => :optional
-  depends_on "python"  => :optional
   depends_on "qcdnum"  => :optional
   depends_on "root"    => :optional
   depends_on "yoda"    => :optional
 
   def am_opt(pkg)
     (build.with? pkg) ? "--with-#{pkg}=#{Formula[pkg].opt_prefix}" : "--without-#{pkg}"
+  end
+
+  def download_pdfs(dest, pdfs)
+    pdfs.each { |pdf| quiet_system "lhapdf", "--pdfdir=#{dest}", "install", pdf }
+    ENV["LHAPDF_DATA_PATH"] = dest
   end
 
   def install
@@ -30,13 +37,17 @@ class Fastnlo < Formula
     args << am_opt("hoppet")
     args << am_opt("yoda")
     args << am_opt("root")
-    args << "--enable-pyext" if build.with? "python"
 
     system "./configure", *args
+    system "make"
+    if build.with? "test"
+      download_pdfs(buildpath/"pdf-sets", %w[CT10nlo])
+      system "make", "check"
+    end
     system "make", "install"
   end
 
   test do
-    system "#{bin}/fnlo-tk-cppread", "-h"
+    system bin/"fnlo-tk-cppread", "-h"
   end
 end
