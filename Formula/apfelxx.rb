25cd9f4152f6cd9a67103f2eff2513bbd355296c
David C Hall <davidchall@users.noreply.github.com>
2022-05-15 22:02:03 -0700

    apfelxx: fix test
    Closes #220.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/apfelxx.rb
+++ Formula/apfelxx.rb
@@ -5,14 +5,19 @@ class Apfelxx < Formula
   sha256 "37ad4121dfa5726ebe09cd2b7cf148976feb063ec6aeae7332ad9adc29c41543"
 
   depends_on "cmake" => :build
-  depends_on "gcc"
+  depends_on "gcc" # for gfortran
 
   def install
     system "cmake", ".", *std_cmake_args
+    system "make"
     system "make", "install"
+
+    prefix.install "tests/dglap_test.cc"
   end
 
   test do
-    system "#{bin}/apfelxx-config", "--dlflags"
+    flags = shell_output(bin/"apfelxx-config --cppflags --ldflags").split
+    system ENV.cxx, "-std=c++11", prefix/"dglap_test.cc", "-o", "test", *flags
+    system "./test"
   end
 end
