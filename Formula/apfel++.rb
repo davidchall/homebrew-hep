d678e6e657d338bc500f66d7ae4f48bcbf5e3df2
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-05-17 20:00:23 +0000

    apfel++: add 4.6.0_1 bottle.

--- Formula/apfel++.rb
+++ Formula/apfel++.rb
@@ -5,6 +5,13 @@ class Apfelxx < Formula
   sha256 "37ad4121dfa5726ebe09cd2b7cf148976feb063ec6aeae7332ad9adc29c41543"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 cellar: :any, monterey: "24473d21312cbc2d01037192071f87b7daba220e2741b58ecd16879b086c8983"
+    sha256 cellar: :any, big_sur:  "e5dabd91b019dcccf51f0a6030c484abd08fca297f52165deadd1be737ef7530"
+    sha256 cellar: :any, catalina: "eada3297fddd84f04a6a9c60dbb7876b4aeb1c002cbd8b449cdefec1ce1ed12a"
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
 
