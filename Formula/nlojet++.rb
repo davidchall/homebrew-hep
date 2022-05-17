5ada8e62725c83000572a69cdb39ab00ce54bc22
David C Hall <davidchall@users.noreply.github.com>
2022-05-17 08:21:31 -0700

    nlojet++: add livecheck

--- Formula/nlojet++.rb
+++ Formula/nlojet++.rb
@@ -4,6 +4,11 @@ class Nlojetxx < Formula
   url "https://desy.de/~znagy/hep-programs/nlojet++/nlojet++-4.1.3.tar.gz"
   sha256 "176d081bbc8c167cdd4516959cbcb2b9ba8f5515f503d29380deee2f67b10ea3"
 
+  livecheck do
+    url :homepage
+    regex(/href=.*?nlojet\+\+[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   patch :p1 do
     url "https://gist.githubusercontent.com/veprbl/e404103016ba819d580b/raw/917c57e8cb47b025b8eef1cf6d74174540fb3ccd/nlojet_clang_fix.patch"
     sha256 "4f7b10953b95b8568f10e7db674628f22e7198b158ed158e870a428b9f0b02d1"
