43d0e87189153d18db1619e4f854e99da61f8e1f
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-05-18 00:49:26 +0000

    applgrid: add 1.6.27 bottle.

--- Formula/applgrid.rb
+++ Formula/applgrid.rb
@@ -10,6 +10,13 @@ class Applgrid < Formula
     regex(/href=.*?applgrid[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 cellar: :any, monterey: "19c2fe49f23f6a9f24440e272cfc7e490e83eda4bf1a0fd89d57605a8ce30548"
+    sha256 cellar: :any, big_sur:  "971209fb409b6577c974b0909de087e662e1fb6f8f5bb6b89d76ce1a24d596ca"
+    sha256 cellar: :any, catalina: "59c4590544e40fe820c342954220e27330fafcb910bf0de399b08fd1be2e5c0e"
+  end
+
   depends_on "gcc" # for gfortran
   depends_on "hoppet" => :recommended
   depends_on "lhapdf" => :recommended
