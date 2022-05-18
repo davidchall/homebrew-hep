74c4dcb2d965537b24fa0ceaa39032accb524a7a
David C Hall <davidchall@users.noreply.github.com>
2022-05-18 08:38:38 -0700

    openloops: 2.1.2

--- Formula/openloops.rb
+++ Formula/openloops.rb
@@ -1,15 +1,16 @@
 class Openloops < Formula
   desc "Fully automated implementation of the Open Loops algorithm"
   homepage "https://openloops.hepforge.org"
-  url "https://www.hepforge.org/archive/openloops/OpenLoops-1.3.1.tar.gz"
-  sha256 "b7610ca62edfab55d0990e94e38048b62b4af0c6b01830b44f40795cd86cb665"
+  url "https://openloops.hepforge.org/downloads?f=OpenLoops-2.1.2.tar.gz"
+  sha256 "f52575cae3d70b6b51a5d423e9cd0e076ed5961afcc015eec00987e64529a6ae"
 
   livecheck do
     url "https://openloops.hepforge.org/downloads"
     regex(/href=.*?OpenLoops[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  depends_on "scons" => :build
+  depends_on arch: :x86_64 # https://github.com/davidchall/homebrew-hep/issues/203
+  depends_on "scons"
   depends_on "gcc" # for gfortran
 
   patch :DATA
