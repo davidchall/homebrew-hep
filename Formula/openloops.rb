d3a0aab877922943dc261371057564c9aeabab01
David C Hall <davidchall@users.noreply.github.com>
2022-05-18 08:38:38 -0700

    openloops 2.1.2
    * openloops: 2.1.2
    * reorder
    * use cfg file
    * scons build dep
    * Update openloops.rb
    * Fix rpath
      https://github.com/SCons/scons/wiki/usingorigin
    * patch
    * help scons merge rpath args
    * Update openloops.rb
    * Update openloops.rb
    * Update openloops.rb
    * Update openloops.rb
    * Update openloops.rb
    * Update openloops.rb
    * Update openloops.rb
    
    Closes #231.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/openloops.rb
+++ Formula/openloops.rb
@@ -1,8 +1,8 @@
 class Openloops < Formula
   desc "Fully automated implementation of the Open Loops algorithm"
   homepage "https://openloops.hepforge.org"
-  url "https://www.hepforge.org/archive/openloops/OpenLoops-1.3.1.tar.gz"
-  sha256 "b7610ca62edfab55d0990e94e38048b62b4af0c6b01830b44f40795cd86cb665"
+  url "https://openloops.hepforge.org/downloads?f=OpenLoops-2.1.2.tar.gz"
+  sha256 "f52575cae3d70b6b51a5d423e9cd0e076ed5961afcc015eec00987e64529a6ae"
 
   livecheck do
     url "https://openloops.hepforge.org/downloads"
@@ -10,6 +10,7 @@ class Openloops < Formula
   end
 
   depends_on "scons" => :build
+  depends_on arch: :x86_64 # https://github.com/davidchall/homebrew-hep/issues/203
   depends_on "gcc" # for gfortran
 
   patch :DATA
@@ -50,3 +51,16 @@ index 5f068c9..e3e09f7 100755
  fi
 
  #####################
+
+diff --git a/SConstruct b/SConstruct
+index 1111ea7..d0e9a0b 100644
+--- a/SConstruct
++++ b/SConstruct
+@@ -387,6 +387,7 @@ env = Environment(tools = ['default', 'textfile'] + [config['fortran_tool']],
+                   LINKFLAGS = config['link_flags'],
+                   LIBPATH = [config['generic_lib_dir']],
+                   DOLLAR = '\$$',
++                  RPATHPREFIX = "-rpath,",
+                   RPATH = [HashableLiteral('\$$ORIGIN')],
+                   F90 = config['fortran_compiler'],
+                   FORTRAN = config['fortran_compiler'],
