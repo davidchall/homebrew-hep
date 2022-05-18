eafb1fc22610376b6a5b77784c6acc5bf4150ea0
David C Hall <davidchall@users.noreply.github.com>
2022-05-17 21:32:07 -0700

    jetvheto: 3.0.0

--- Formula/jetvheto.rb
+++ Formula/jetvheto.rb
@@ -1,34 +1,41 @@
 class Jetvheto < Formula
   desc "NNLL resummation for jet-veto efficiencies and cross sections"
   homepage "https://jetvheto.hepforge.org"
-  url "https://jetvheto.hepforge.org/downloads/JetVHeto-1.0.0.tgz"
-  sha256 "1e9673439fa9df840e09c0cb94329d52b38e4b5ef49c6f5f6de2388f574cfa3e"
+  url "https://jetvheto.hepforge.org/downloads/?f=JetVHeto-3.0.0.tgz"
+  sha256 "c3eaefd2ff154df70ed7d1ead053c7419e82ec854f2124a3d58585193866a40f"
+  license "GPL-3.0-only"
 
   livecheck do
     url "https://jetvheto.hepforge.org/downloads/"
     regex(/href=.*?JetVHeto[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  depends_on "chaplin"
+  depends_on "gcc" # for gfortran
   depends_on "hoppet"
   depends_on "lhapdf"
 
+  def download_pdfs(dest, pdfs)
+    pdfs.each { |pdf| quiet_system "lhapdf", "--pdfdir=#{dest}", "install", pdf }
+    ENV["LHAPDF_DATA_PATH"] = dest
+  end
+
   def install
+    inreplace "Makefile", /^CHAPLIN=.+/, "CHAPLIN=#{Formula["chaplin"].opt_lib}"
     system "make"
     bin.install "jetvheto"
-    share.install "benchmarks"
-    share.install "fixed-order"
+    share.install Dir["*fixed-order*"]
+    share.install Dir["*benchmarks*"]
+    share.install "python"
+    share.install "scripts"
   end
 
   test do
-    # Require a PDF set
-    curl "-O", "http://www.hepforge.org/archive/lhapdf/pdfsets/6.0/MSTW2008nnlo68cl.tar.gz"
-    system "tar", "xzf", "MSTW2008nnlo68cl.tar.gz"
-    ENV["LHAPDF_DATA_PATH"] = Dir.pwd
+    download_pdfs(testpath/"pdf-sets", %w[MSTW2008nnlo68cl])
 
-    cp "#{share}/fixed-order/H125-LHC8-R04-xmur050-xmuf050-log.fxd", "input.fxd"
+    cp share/"fixed-order/H125-LHC8-R04-xmur050-xmuf050-log.fxd", "input.fxd"
     inreplace "input.fxd", "MSTW2008nnlo90cl.LHgrid", "MSTW2008nnlo68cl"
 
-    system "#{bin}/jetvheto", "-in", "input.fxd", "-out", "output.res", "-xQ", "0.05"
-    ohai "Successfully resummed logs for H production at the LHC"
+    system bin/"jetvheto", "-in", "input.fxd", "-out", "output.res", "-xQ", "0.05"
   end
 end
