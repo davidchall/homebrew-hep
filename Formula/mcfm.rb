e88de271b4a243f3117a8cae75d1023e1a4b9ed6
David C Hall <davidchall@users.noreply.github.com>
2022-05-19 23:19:56 -0700

    fix gfortran linking

--- Formula/mcfm.rb
+++ Formula/mcfm.rb
@@ -11,7 +11,6 @@ class Mcfm < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on arch: :x86_64 # https://github.com/davidchall/homebrew-hep/issues/203
   depends_on "gcc"
   depends_on "lhapdf"
 
@@ -21,17 +20,28 @@ class Mcfm < Formula
   fails_with gcc: "6"
 
   def install
+    gfortran_lib = Formula["gcc"].opt_lib/"gcc"/Formula["gcc"].version_suffix
+    inreplace "lib/qcdloop-2.0.5/CMakeLists.txt",
+      "target_link_libraries(qcdloop_shared)",
+      "target_link_libraries(qcdloop_shared -L#{gfortran_lib} -lquadmath)"
+    inreplace "lib/qcdloop-2.0.5/CMakeLists.txt",
+      "target_link_libraries(qcdloop_static)",
+      "target_link_libraries(qcdloop_static -L#{gfortran_lib} -lquadmath)"
+
     mkdir "../build" do
       system "cmake", buildpath, *std_cmake_args, "-Duse_internal_lhapdf=OFF"
       system "make"
       system "make", "install"
 
+      bin.install "Bin/mcfm_omp"
+      pkgshare.install Dir["Bin/*"]
       doc.install Dir["Doc/*.pdf"]
     end
   end
 
   test do
-    system bin/"mcfm_omp", share/"input.DAT"
+    download_pdfs(buildpath/"pdf-sets", "CT14nnlo")
+    system bin/"mcfm_omp", pkgshare/"input.ini"
     system "ls"
     assert_predicate testpath/"W_only_nlo_CT14.NN_80___80___13TeV.top", :exist?
   end
