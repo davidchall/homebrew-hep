afb60452a74200e5ed0f3c95cc4a089f1ddd7eda
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-08-20 22:16:16 +0000

    applgrid: update 1.6.31 bottle.

--- Formula/applgrid.rb
+++ Formula/applgrid.rb
@@ -12,9 +12,9 @@ class Applgrid < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/davidchall/hep"
-    sha256 cellar: :any, monterey: "b2e7cc54c0371d649ab23120eb916239ead89004de77e0e345cfaf1c5d963a60"
-    sha256 cellar: :any, big_sur:  "dd2847ec25410db823b7e02ef8c3b4e6b9ea911a6c867bc90bb4d8547d926a1a"
-    sha256 cellar: :any, catalina: "11bad5decf5f98bb6341bc16c576e662e788d170076591e1576e974d99d07af4"
+    sha256 cellar: :any, monterey: "2e3e3ffe97c23eb24c3f395ee2a06b3b9e8592ab7bfd1667509713b5e0ca5bb8"
+    sha256 cellar: :any, big_sur:  "4ab060b1878b06eaaee47d99f34f0d68f48042641f7b9891e99aaf9da2067597"
+    sha256 cellar: :any, catalina: "022ed6893763da687242f0d4b08cb59a606217a8dd07cec6556b9e5db72088fa"
   end
 
   depends_on "gcc" # for gfortran
