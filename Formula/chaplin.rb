bd674157dddf1671b0277209b3b2ac7bf6c8b8b4
David C Hall <davidchall@users.noreply.github.com>
2022-09-11 22:21:15 -0700

    chaplin: rebuild bottles

--- Formula/chaplin.rb
+++ Formula/chaplin.rb
@@ -3,6 +3,7 @@ class Chaplin < Formula
   homepage "https://chaplin.hepforge.org"
   url "https://chaplin.hepforge.org/code/chaplin-1.2.tar"
   sha256 "f17c2d985fd4e4ce36cede945450416d3fa940af68945c91fa5d3ca1d76d4b49"
+  revision 1
 
   livecheck do
     url "https://chaplin.hepforge.org/downloads"
