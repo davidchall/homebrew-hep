512721da39e0c8c4334e0f55e723617517e81ea2
David C Hall <davidchall@users.noreply.github.com>
2022-09-11 22:04:12 -0700

    apfel++: rebuild bottles

--- Formula/apfel++.rb
+++ Formula/apfel++.rb
@@ -3,7 +3,7 @@ class Apfelxx < Formula
   homepage "https://github.com/vbertone/apfelxx"
   url "https://github.com/vbertone/apfelxx/archive/4.6.0.tar.gz"
   sha256 "37ad4121dfa5726ebe09cd2b7cf148976feb063ec6aeae7332ad9adc29c41543"
-  revision 1
+  revision 2
 
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
