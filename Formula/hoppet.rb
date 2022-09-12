a0f691b44f25aaaeb5d629df04787e12b9975b4c
David C Hall <1804856+davidchall@users.noreply.github.com>
2022-09-12 05:32:50 +0000

    hoppet: add 1.2.0_1 bottle.

--- Formula/hoppet.rb
+++ Formula/hoppet.rb
@@ -10,6 +10,13 @@ class Hoppet < Formula
     regex(/href=.*?hoppet[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 cellar: :any_skip_relocation, monterey: "edcc774e58323d48b7bd32249149aa004fdf1b5f6d35eaf7063ebfe1be55ca0e"
+    sha256 cellar: :any_skip_relocation, big_sur:  "ad51ad289809efbeeafbda083929c16a8fda7b3d61adda228220a6527eceed9c"
+    sha256 cellar: :any_skip_relocation, catalina: "24a394a8a4e59c662d76267c4b8254314cb2fc5e64705154dad1ea1e8bf4ec29"
+  end
+
   option "with-test", "Test during installation"
 
   depends_on "gcc" # for gfortran
