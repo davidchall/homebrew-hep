b9ad6415d30999c8e28c24b3bea82a47bf8fe826
David C Hall <davidchall@users.noreply.github.com>
2022-09-12 18:42:47 -0700

    qcdnum: rebuild bottles

--- Formula/qcdnum.rb
+++ Formula/qcdnum.rb
@@ -5,6 +5,7 @@ class Qcdnum < Formula
   version "18.0.0"
   sha256 "e108f926b7840352e4080ba71914d3403ed8118364f87710e221fdec320ee200"
   license "GPL-3.0-or-later"
+  revision 1
 
   livecheck do
     url "https://www.nikhef.nl/~h24/qcdnum-files/download/"
