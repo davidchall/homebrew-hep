5d72b2bb3b1f76f5294d5b514c23c3a4dd22837c
David C Hall <davidchall@users.noreply.github.com>
2022-11-28 01:01:06 +0000

    sherpa 2.2.13
    * sherpa 2.2.13
    * Use C++14 (required by rivet)
    * Homebrew doesn't recognize ENV.cxx14
    
    Closes #290.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/sherpa.rb
+++ Formula/sherpa.rb
@@ -1,10 +1,9 @@
 class Sherpa < Formula
   desc "Monte Carlo event generator"
   homepage "https://sherpa-team.gitlab.io"
-  url "https://sherpa.hepforge.org/downloads/?f=SHERPA-MC-2.2.12.tar.gz"
-  sha256 "529464775ca828b9dc965110526f340de743b6003c1e6f8abe2fc14550aed001"
+  url "https://sherpa.hepforge.org/downloads/?f=SHERPA-MC-2.2.13.tar.gz"
+  sha256 "148a339b6026c5a10b20388b04dc7c46d502cb57c88d31ad9e916201c29c6571"
   license "GPL-2.0-only"
-  revision 1
 
   livecheck do
     url "https://sherpa.hepforge.org/downloads"
@@ -28,8 +27,6 @@ class Sherpa < Formula
   depends_on "root"    => :optional
 
   def install
-    ENV.cxx11
-
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
@@ -43,6 +40,9 @@ class Sherpa < Formula
     args << "--enable-lhapdf=#{Formula["lhapdf"].opt_prefix}"   if build.with? "lhapdf"
     args << "--enable-rivet=#{Formula["rivet"].opt_prefix}"     if build.with? "rivet"
     args << "--enable-root=#{Formula["root"].opt_prefix}"       if build.with? "root"
+
+    # rivet requires C++14
+    ENV.append "CXXFLAGS", "-std=c++14"
 
     system "./configure", *args
     system "make"
