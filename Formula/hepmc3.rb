601440746ff5b72c5fa3045de717b23ade1c0833
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-04-12 17:41:41 +0200

    No whitespace

--- Formula/hepmc3.rb
+++ Formula/hepmc3.rb
@@ -21,7 +21,7 @@ class Hepmc3 < Formula
   depends_on "cmake" => [:build, :test]
   depends_on "coreutils" # HepMC3-config uses greadlink
   depends_on "python@3.10"
-  depends_on "protobuf" => :optional  
+  depends_on "protobuf" => :optional
   depends_on "root" => :optional
 
   def python
