ef8a650546cef94c1be07dab2f625cb928f44a2d
David C Hall <davidchall@users.noreply.github.com>
2023-10-11 20:38:40 -0700

    apfel++: rebuild bottles
    Closes #340.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/apfel++.rb
+++ Formula/apfel++.rb
@@ -4,12 +4,6 @@ class Apfelxx < Formula
   url "https://github.com/vbertone/apfelxx/archive/4.7.0.tar.gz"
   sha256 "01eff0c14e16a83ad25e34159985d7ee33264822585495003e090db57c66a1ac"
 
-  bottle do
-    root_url "https://ghcr.io/v2/davidchall/hep"
-    sha256 cellar: :any, monterey: "cfb805b7a0b8538f312b53951b41ebbbbb1679806a9fb8155ef430267a0e6ece"
-    sha256 cellar: :any, big_sur:  "3ad6a6559b04a6982914febf07b5815e5555ce82055c1cd255a5367450e4e394"
-  end
-
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
 
