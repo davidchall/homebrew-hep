b8c1b3afbf2a30c3ada3200772052beedcfe4014
David C Hall <davidchall@users.noreply.github.com>
2023-10-25 19:22:06 -0700

    Fix audit error

--- Formula/apfel++.rb
+++ Formula/apfel++.rb
@@ -1,7 +1,7 @@
 class Apfelxx < Formula
   desc "Object oriented rewriting of the APFEL evolution code"
   homepage "https://github.com/vbertone/apfelxx"
-  url "https://github.com/vbertone/apfelxx/archive/4.7.0.tar.gz"
+  url "https://github.com/vbertone/apfelxx/archive/refs/tags/4.7.0.tar.gz"
   sha256 "01eff0c14e16a83ad25e34159985d7ee33264822585495003e090db57c66a1ac"
 
   bottle do
