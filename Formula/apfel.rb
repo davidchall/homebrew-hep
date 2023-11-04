b7002260e398ba47ef8cf4527d9bae95b7b414ab
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-11-04 22:00:03 +0100

    Add a patch

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -4,6 +4,9 @@ class Apfel < Formula
   url "https://github.com/scarrazza/apfel/archive/refs/tags/3.0.6.tar.gz"
   sha256 "7063c9eee457e030b97926ac166cdaedd84625b31397e1dfd01ae47371fb9f61"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "gcc" # for gfortran
   depends_on "lhapdf"
 
@@ -15,6 +18,7 @@ class Apfel < Formula
       --disable-pywrap
       --enable-lhapdf
     ]
+    system "autoreconf", "-i"
     system "./configure", *args
     system "make"
     system "make", "install"
@@ -28,3 +32,14 @@ class Apfel < Formula
     system "./LuminosityCxx"
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+@@ -85,7 +85,6 @@
+ AC_ARG_ENABLE(pywrap, [AS_HELP_STRING([--disable-pywrap],[don't build Python module (default=build)])],
+   [], [enable_pywrap=yes])   
+ 
+-enable_pywrap=yes
+ AZ_PYTHON_DEFAULT
+ ## Basic Python checks
+ if test x$enable_pywrap == xyes; then
