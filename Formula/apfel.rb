ea32de495183a73df6395013f44230cf29bada4b
andriish <andriish@users.noreply.github.com>
2023-11-05 22:15:23 +0100

    Update Formula/apfel.rb
    Co-authored-by: David C Hall <davidchall@users.noreply.github.com>

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -25,12 +25,12 @@ class Apfel < Formula
     system "make"
     system "make", "install"
 
-    prefix.install "examples/LuminosityCxx.cc"
+    prefix.install "examples"
   end
 
   test do
     flags = shell_output(bin/"apfel-config --cxxflags --ldflags").split
-    system ENV.cxx, "-std=c++11", prefix/"LuminosityCxx.cc", "-o", "LuminosityCxx", *flags
+    system ENV.cxx, "-std=c++11", prefix/"examples/LuminosityCxx.cc", "-o", "LuminosityCxx", *flags
     system "./LuminosityCxx"
   end
 end
