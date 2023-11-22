94d56e84abb9bb39b01e4be98b85537e250d339d
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-22 10:22:16 +0100

    apfel: 3.1.0
    Closes #350.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -1,72 +1,38 @@
 class Apfel < Formula
   desc "PDF Evolution Library"
   homepage "https://github.com/scarrazza/apfel"
-  url "https://github.com/scarrazza/apfel/archive/refs/tags/3.0.6.tar.gz"
-  sha256 "7063c9eee457e030b97926ac166cdaedd84625b31397e1dfd01ae47371fb9f61"
+  url "https://github.com/scarrazza/apfel/archive/refs/tags/3.1.0.tar.gz"
+  sha256 "0a7e693536de6a9a84504c221a77cd8154fd939c3332d21dfce74432d468f4fd"
   license "GPL-3.0-only"
 
-  bottle do
-    root_url "https://ghcr.io/v2/davidchall/hep"
-    sha256 ventura:  "9c8b136bf1403649bf27f6ac85cd8fdc92a2b8768bfa0eb18e70ea7f5c00f157"
-    sha256 monterey: "783a9a7b763ec253e168d8ffe7ed75f1985f882a8b3789dd89684a3b4b68b6f1"
-  end
+  option "with-test", "Test during installation"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "cmake" => [:build, :test]
+  depends_on "swig" => :build
   depends_on "gcc" # for gfortran
   depends_on "lhapdf"
+  depends_on "python@3.10"
 
-  patch :DATA
+  def python
+    "python3.10"
+  end
 
   def install
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --disable-pywrap
-      --enable-lhapdf
-    ]
-    system "autoreconf", "-i"
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    mkdir "../build" do
+      args = %W[
+        -DCMAKE_INSTALL_PREFIX=#{prefix}
+        -DAPFEL_Python_SITEARCH=#{prefix/Language::Python.site_packages(python)}
+      ]
 
-    prefix.install "examples"
+      system "cmake", buildpath, *args
+      system "make"
+      system "make", "test" if build.with? "test"
+      system "make", "install"
+    end
   end
 
   test do
-    flags = shell_output(bin/"apfel-config --cxxflags --ldflags").split
-    system ENV.cxx, "-std=c++11", prefix/"examples/LuminosityCxx.cc", "-o", "LuminosityCxx", *flags
-    system "./LuminosityCxx"
+    assert_equal prefix.to_s, shell_output(bin/"apfel-config --prefix").strip
+    system Formula["python@3.10"].opt_bin/python, "-c", "import apfel"
   end
 end
-
-__END__
-diff --git a/Makefile.am b/Makefile.am
-index 6e792d8..06c0f43 100644
---- a/Makefile.am
-+++ b/Makefile.am
-@@ -1,6 +1,9 @@
- ACLOCAL_AMFLAGS = -I m4
- 
--SUBDIRS = include ccwrap src lib examples pywrap doc bin
-+SUBDIRS = include ccwrap src lib examples  doc bin
-+if ENABLE_PYWRAP
-+SUBDIRS += pywrap
-+endif
- dist_doc_DATA = README
- dist_pkgdata_DATA = src/HELL/data/*
- 
-diff --git a/configure.ac b/configure.ac
-index 4b1845d..22d6dcf 100644
---- a/configure.ac
-+++ b/configure.ac
-@@ -85,7 +85,6 @@ AM_CONDITIONAL(ENABLE_LHAPDF, [test x$enable_lhapdf == xyes])
- AC_ARG_ENABLE(pywrap, [AS_HELP_STRING([--disable-pywrap],[don't build Python module (default=build)])],
-   [], [enable_pywrap=yes])   
- 
--enable_pywrap=yes
- AZ_PYTHON_DEFAULT
- ## Basic Python checks
- if test x$enable_pywrap == xyes; then
