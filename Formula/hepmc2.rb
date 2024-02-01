cdbb6ba8ac3c42518b18e2f670cf919df7af4a94
David C Hall <1804856+davidchall@users.noreply.github.com>
2024-02-01 17:00:28 +0000

    hepmc2: add 2.06.11 bottle.

--- Formula/hepmc2.rb
+++ Formula/hepmc2.rb
@@ -5,6 +5,13 @@ class Hepmc2 < Formula
   sha256 "ceaced62d39e4e2a1469fa2f20662d4d370279b3209930250766db02f44ae8de"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    rebuild 3
+    sha256 cellar: :any, arm64_sonoma: "a100ed1d92ff3f3f703103c1d704f55858c423cd1c91d5eef02c5765289c2c84"
+    sha256 cellar: :any, ventura:      "116cb869287b55c50c7e7fce6e6a6ec0a92a5ad21fd6ae98f8f1fc6325ad5519"
+  end
+
   option "with-test", "Test during installation"
 
   depends_on "cmake" => :build
