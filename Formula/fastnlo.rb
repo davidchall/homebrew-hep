25e5a58b677ee08c1b07b9392278aad7ccdc01e5
David C Hall <davidchall@users.noreply.github.com>
2024-02-26 22:11:52 -0800

    Update fastnlo.rb
    Closes #376.
    
    Signed-off-by: David C Hall <1804856+davidchall@users.noreply.github.com>

--- Formula/fastnlo.rb
+++ Formula/fastnlo.rb
@@ -7,11 +7,18 @@ class Fastnlo < Formula
   version "2.5.0"
   sha256 "eda20c6023a41c2ff5ec21d88847ebc6fb522714c38afad2ec1437d9c1a2132d"
   license "GPL-3.0-only"
-  revision 1
+  revision 2
 
   livecheck do
     url :homepage
     regex(%r{href=.*?code/v\d+/fastnlo_toolkit[._-]v?(\d+(?:\.\d+)+)-\d+\.t}i)
+  end
+
+  bottle do
+    root_url "https://ghcr.io/v2/davidchall/hep"
+    sha256 cellar: :any, monterey: "21b03b289194d24aff0f7bec0cbfea4b4ecf74889ce1eb2643717f2deb0c8b4b"
+    sha256 cellar: :any, big_sur:  "f3e40a12ffdf6d182d29a843f2a0cc0d8667caaaecc5035d5ab85d809fd1abf7"
+    sha256 cellar: :any, catalina: "ab30fb3cd1097ede185b2d3cf9c794fc094d3490d2212f6789948daa0493abab"
   end
 
   option "with-test", "Test during installation"
@@ -23,10 +30,6 @@ class Fastnlo < Formula
   depends_on "root"    => :optional
   depends_on "yoda"    => :optional
 
-  def am_opt(pkg)
-    build.with? pkg ? "--with-#{pkg}=#{Formula[pkg].opt_prefix}" : "--without-#{pkg}"
-  end
-
   def install
     args = %W[
       --disable-dependency-tracking
@@ -35,11 +38,11 @@ class Fastnlo < Formula
       --with-lhapdf=#{Formula["lhapdf"].opt_prefix}
     ]
 
-    args << am_opt("fastjet")
-    args << am_opt("qcdnum")
-    args << am_opt("hoppet")
-    args << am_opt("yoda")
-    args << am_opt("root")
+    args << "--with-fastjet=#{Formula["fastjet"].opt_prefix}" if build.with? "fastjet"
+    args << "--with-hoppet=#{Formula["hoppet"].opt_prefix}"   if build.with? "hoppet"
+    args << "--with-qcdnum=#{Formula["qcdnum"].opt_prefix}"   if build.with? "qcdnum"
+    args << "--with-root=#{Formula["root"].opt_prefix}"       if build.with? "root"
+    args << "--with-yoda=#{Formula["yoda"].opt_prefix}"       if build.with? "yoda"
 
     system "./configure", *args
     system "make"
