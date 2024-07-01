846fa70e1e46e9149ebb601def8936d7b86ee5d5
David C Hall <davidchall@users.noreply.github.com>
2024-07-01 01:01:19 +0000

    fjcontrib 1.055

--- Formula/fjcontrib.rb
+++ Formula/fjcontrib.rb
@@ -1,8 +1,8 @@
 class Fjcontrib < Formula
   desc "Package of contributed add-ons to FastJet"
   homepage "https://fastjet.hepforge.org/contrib/"
-  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.054.tar.gz"
-  sha256 "1ef922d4c45863e5fe7a3b64dc441703db6b1c2cc92d4160125dc629b05ac331"
+  url "https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.055.tar.gz"
+  sha256 "d9aa46560fdfd85082f202a5a9ce64768fe0c598660f013206a8c9c17ecd0f36"
 
   livecheck do
     url "https://fastjet.hepforge.org/contrib/downloads"
