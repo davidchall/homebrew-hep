03b4d1483dd2d71e3d61d45a71ab9ddd2abbf43a
David C Hall <davidchall@users.noreply.github.com>
2024-11-04 01:06:31 +0000

    rivet 4.0.2

--- Formula/rivet.rb
+++ Formula/rivet.rb
@@ -3,8 +3,8 @@ class Rivet < Formula
 
   desc "Monte Carlo analysis system"
   homepage "https://rivet.hepforge.org"
-  url "https://rivet.hepforge.org/downloads/?f=Rivet-3.1.10.tar.gz"
-  sha256 "534389243e7fa3a407a08ac00a4cac9a133d03aedb0b334c19f4edc5889db343"
+  url "https://rivet.hepforge.org/downloads/?f=Rivet-4.0.2.tar.gz"
+  sha256 "7a3bed88d95bea5ae2cc6b79808b4c91cd11ec840e3308de5cb34685a1470363"
   license "GPL-3.0-only"
 
   livecheck do
