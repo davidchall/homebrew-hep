a0246dc5c5ee0d859e27c2ff69bfbe1a01f62e54
David C Hall <davidchall@users.noreply.github.com>
2025-03-31 01:13:15 +0000

    applgrid 1.6.38

--- Formula/applgrid.rb
+++ Formula/applgrid.rb
@@ -1,8 +1,8 @@
 class Applgrid < Formula
   desc "Quickly reproduce NLO calculations with any input PDFs"
   homepage "https://applgrid.hepforge.org"
-  url "https://applgrid.hepforge.org/downloads?f=applgrid-1.6.36.tgz"
-  sha256 "2dc23844671284b4cf9f4b69f317ef68a1530b4b1fb093a31395b35aac9fcf1c"
+  url "https://applgrid.hepforge.org/downloads?f=applgrid-1.6.38.tgz"
+  sha256 "0f2a1aa49d3a6ebc00241da8a8bcf54667a98372b74f99021a181290909519e4"
   license "GPL-3.0-only"
 
   livecheck do
